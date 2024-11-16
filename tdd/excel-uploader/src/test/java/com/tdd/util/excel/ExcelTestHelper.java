dfab2a55d3bf9808591e59fb75664cd05fb12237
currenjin <hyun0524e@naver.com>
2024-11-16 18:48:42 +0900

    Test: (disabled) valid excel

--- tdd/excel-uploader/src/test/java/com/tdd/util/excel/ExcelTestHelper.java
+++ tdd/excel-uploader/src/test/java/com/tdd/util/excel/ExcelTestHelper.java
@@ -2,6 +2,7 @@ package com.tdd.util.excel;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -12,22 +13,58 @@ import org.springframework.web.multipart.MultipartFile;
 public class ExcelTestHelper {
 	public static MultipartFile createEmptyExcelFile() throws IOException {
 		try (XSSFWorkbook workbook = new XSSFWorkbook()) {
-			XSSFSheet sheet = workbook.createSheet("Sheet1");
+			XSSFSheet sheet = createSheet(workbook);
 
-			XSSFRow headerRow = sheet.createRow(0);
-			headerRow.createCell(0).setCellValue("기사명");
-			headerRow.createCell(1).setCellValue("전화번호");
+			setHeaderRow(sheet);
 
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			workbook.write(bos);
-			byte[] bytes = bos.toByteArray();
+			byte[] bytes = toByteArray(workbook);
 
-			return new MockMultipartFile(
-				"file",
-				"test.xlsx",
-				"multipart/form-data",
-				bytes
-			);
+			return convertMockMultipartFile(bytes);
 		}
+	}
+
+	public static MultipartFile createExcelFile(List<String[]> data) throws IOException {
+		try (XSSFWorkbook workbook = new XSSFWorkbook()) {
+			XSSFSheet sheet = createSheet(workbook);
+
+			setHeaderRow(sheet);
+
+			for (int i = 0; i < data.size(); i++) {
+				XSSFRow dataRow = sheet.createRow(i + 1);
+				String[] rowData = data.get(i);
+				for (int j = 0; j < rowData.length; j++) {
+					dataRow.createCell(j).setCellValue(rowData[j]);
+				}
+			}
+
+			byte[] bytes = toByteArray(workbook);
+
+			return convertMockMultipartFile(bytes);
+		}
+	}
+
+	private static MockMultipartFile convertMockMultipartFile(byte[] bytes) {
+		return new MockMultipartFile(
+			"file",
+			"test.xlsx",
+			"multipart/form-data",
+			bytes
+		);
+	}
+
+	private static byte[] toByteArray(XSSFWorkbook workbook) throws IOException {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		workbook.write(bos);
+		return bos.toByteArray();
+	}
+
+	private static XSSFSheet createSheet(XSSFWorkbook workbook) {
+		return workbook.createSheet("Sheet1");
+	}
+
+	private static void setHeaderRow(XSSFSheet sheet) {
+		XSSFRow headerRow = sheet.createRow(0);
+		headerRow.createCell(0).setCellValue("기사명");
+		headerRow.createCell(1).setCellValue("전화번호");
 	}
 }
--- tdd/excel-uploader/src/test/java/com/tdd/util/excel/ExcelValidatorTest.java
+++ tdd/excel-uploader/src/test/java/com/tdd/util/excel/ExcelValidatorTest.java
@@ -3,8 +3,11 @@ package com.tdd.util.excel;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,7 +30,17 @@ class ExcelValidatorTest {
 	}
 
 	@Test
-	void validate_for_valid_excel() {
-		ValidationResult actual = ExcelValidator.validate(EMPTY_EXCEL);
+	@Disabled
+	void validate_for_valid_excel() throws IOException {
+		List<String[]> testData = Arrays.asList(
+			new String[]{"홍길동", "010-1234-5678"},
+			new String[]{"김철수", "010-8765-4321"}
+		);
+		MultipartFile validTwoRowExcel = ExcelTestHelper.createExcelFile(testData);
+
+		ValidationResult actual = ExcelValidator.validate(validTwoRowExcel);
+
+		assertEquals(2, actual.getValidResults());
+		assertEquals(0, actual.getInvalidResults());
 	}
 }
