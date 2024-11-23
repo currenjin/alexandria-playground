90c1eaed0bf9aafe958f304c88d1193b668f3799
currenjin <hyun0524e@naver.com>
2024-11-23 21:31:41 +0900

    SpringDataJdbc: Init project

---
+++ spring-data-jdbc/src/test/java/com/currenjin/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.currenjin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
