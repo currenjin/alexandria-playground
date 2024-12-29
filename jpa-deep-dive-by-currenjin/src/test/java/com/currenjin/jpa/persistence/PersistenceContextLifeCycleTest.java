46b8eb2e5e4cbfd3ee4c3b419c454b7798d55b49
currenjin <hyun0524e@naver.com>
2024-12-29 20:13:56 +0900

    JpaDeepDive: Unused import

--- jpa-deep-dive-by-currenjin/src/test/java/com/currenjin/jpa/persistence/PersistenceContextLifeCycleTest.java
+++ jpa-deep-dive-by-currenjin/src/test/java/com/currenjin/jpa/persistence/PersistenceContextLifeCycleTest.java
@@ -1,6 +1,5 @@
 package com.currenjin.jpa.persistence;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
