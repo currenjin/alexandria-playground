601dc7c21abd2176a9ae946b7b7a379852fda32a
currenjin <hyun0524e@naver.com>
2025-03-26 21:26:24 +0900

    JvmSimpleInterpreter: Stack, Constant

--- jvm-simple-interpreter/src/main/java/com/currenjin/jvm/SimpleJVMInterpreter.java
+++ jvm-simple-interpreter/src/main/java/com/currenjin/jvm/SimpleJVMInterpreter.java
@@ -1,9 +1,36 @@
 package com.currenjin.jvm;
 
+import java.awt.*;
+import java.util.Stack;
+
 public class SimpleJVMInterpreter {
+    private static final byte ICONST_0 = 0x03;
+
     private final byte[] bytecode;
+    private int pc = 0;
+    private Stack<Integer> operandStack = new Stack<>();
 
     public SimpleJVMInterpreter(byte[] bytecode) {
         this.bytecode = bytecode;
+    }
+
+    public Stack<Integer> getOperandStack() {
+        Stack<Integer> copy = new Stack<>();
+        copy.addAll(this.operandStack);
+        return copy;
+    }
+
+    public void execute() {
+        while (pc < bytecode.length) {
+            byte opcode = bytecode[pc++];
+
+            switch (opcode) {
+                case ICONST_0:
+                    operandStack.push(0);
+                    break;
+                default:
+                    throw new UnsupportedOperationException("Unsupported opcode: " + opcode);
+            }
+        }
     }
 }
--- jvm-simple-interpreter/src/test/java/com/currenjin/jvm/SimpleJVMInterpreterTest.java
+++ jvm-simple-interpreter/src/test/java/com/currenjin/jvm/SimpleJVMInterpreterTest.java
@@ -2,13 +2,29 @@ package com.currenjin.jvm;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SimpleJVMInterpreterTest {
+    byte[] ICONST_0 = {0x03};
+
     @Test
     void testInterpreterCreation() {
-        byte[] bytecode = {0x03}; // ICONST_0
-        SimpleJVMInterpreter interpreter = new SimpleJVMInterpreter(bytecode);
+        SimpleJVMInterpreter interpreter = new SimpleJVMInterpreter(ICONST_0);
+
         assertNotNull(interpreter, "객체가 생성됩니다.");
+    }
+
+    @Test
+    void testPushConstant() {
+        SimpleJVMInterpreter interpreter = new SimpleJVMInterpreter(ICONST_0);
+
+        interpreter.execute();
+
+        Stack<Integer> stack = interpreter.getOperandStack();
+        assertEquals(1, stack.size(), "스택에 하나의 요소가 있어야 한다");
+        assertEquals(0, stack.peek(), "스택의 최상위 값은 0이어야 한다");
     }
 }
