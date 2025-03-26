7828cba354b411a584860997d606ab0b356c1275
currenjin <hyun0524e@naver.com>
2025-03-26 21:31:00 +0900

    JvmSimpleInterpreter: Multiple constants

--- jvm-simple-interpreter/src/main/java/com/currenjin/jvm/SimpleJVMInterpreter.java
+++ jvm-simple-interpreter/src/main/java/com/currenjin/jvm/SimpleJVMInterpreter.java
@@ -1,10 +1,12 @@
 package com.currenjin.jvm;
 
-import java.awt.*;
 import java.util.Stack;
 
 public class SimpleJVMInterpreter {
     private static final byte ICONST_0 = 0x03;
+    private static final byte ICONST_1 = 0x04;
+    private static final byte ICONST_2 = 0x05;
+    private static final byte ICONST_3 = 0x06;
 
     private final byte[] bytecode;
     private int pc = 0;
@@ -27,6 +29,15 @@ public class SimpleJVMInterpreter {
             switch (opcode) {
                 case ICONST_0:
                     operandStack.push(0);
+                    break;
+                case ICONST_1:
+                    operandStack.push(1);
+                    break;
+                case ICONST_2:
+                    operandStack.push(2);
+                    break;
+                case ICONST_3:
+                    operandStack.push(3);
                     break;
                 default:
                     throw new UnsupportedOperationException("Unsupported opcode: " + opcode);
--- jvm-simple-interpreter/src/test/java/com/currenjin/jvm/SimpleJVMInterpreterTest.java
+++ jvm-simple-interpreter/src/test/java/com/currenjin/jvm/SimpleJVMInterpreterTest.java
@@ -8,23 +8,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SimpleJVMInterpreterTest {
-    byte[] ICONST_0 = {0x03};
 
     @Test
     void testInterpreterCreation() {
-        SimpleJVMInterpreter interpreter = new SimpleJVMInterpreter(ICONST_0);
+        byte[] bytecode = {0x03};
+
+        SimpleJVMInterpreter interpreter = new SimpleJVMInterpreter(bytecode);
 
         assertNotNull(interpreter, "객체가 생성됩니다.");
     }
 
     @Test
     void testPushConstant() {
-        SimpleJVMInterpreter interpreter = new SimpleJVMInterpreter(ICONST_0);
+        byte[] bytecode = {0x03};
+        SimpleJVMInterpreter interpreter = new SimpleJVMInterpreter(bytecode);
 
         interpreter.execute();
 
         Stack<Integer> stack = interpreter.getOperandStack();
         assertEquals(1, stack.size(), "스택에 하나의 요소가 있어야 한다");
         assertEquals(0, stack.peek(), "스택의 최상위 값은 0이어야 한다");
+    }
+
+    @Test
+    void testPushMultipleConstants() {
+        byte[] bytecode = { 0x03, 0x04, 0x05, 0x06 };
+        SimpleJVMInterpreter interpreter = new SimpleJVMInterpreter(bytecode);
+
+        interpreter.execute();
+
+        Stack<Integer> stack = interpreter.getOperandStack();
+        assertEquals(4, stack.size(), "스택에 네 개의 요소가 있어야 한다");
+        assertEquals(3, stack.pop(), "스택의 4번째 값은 3이어야 한다");
+        assertEquals(2, stack.pop(), "스택의 3번째 값은 2이어야 한다");
+        assertEquals(1, stack.pop(), "스택의 2번째 값은 1이어야 한다");
+        assertEquals(0, stack.pop(), "스택의 1번째 값은 0이어야 한다");
     }
 }
