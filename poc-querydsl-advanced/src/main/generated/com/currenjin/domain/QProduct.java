addff1b3ca2c47c3714df50b8f0a964b37c5a086
currenjin <hyun0524e@naver.com>
2025-04-03 09:49:37 +0900

    PocQuerydslAdvanced: init(Domain, Application, Presentation)

---
+++ poc-querydsl-advanced/src/main/generated/com/currenjin/domain/QProduct.java
@@ -0,0 +1,65 @@
+package com.currenjin.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QProduct is a Querydsl query type for Product
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QProduct extends EntityPathBase<Product> {
+
+    private static final long serialVersionUID = -304019508L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QProduct product = new QProduct("product");
+
+    public final QCategory category;
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public final ListPath<OrderItem, QOrderItem> orderItems = this.<OrderItem, QOrderItem>createList("orderItems", OrderItem.class, QOrderItem.class, PathInits.DIRECT2);
+
+    public final NumberPath<Integer> price = createNumber("price", Integer.class);
+
+    public final EnumPath<Product.ProductStatus> status = createEnum("status", Product.ProductStatus.class);
+
+    public final NumberPath<Integer> stockQuantity = createNumber("stockQuantity", Integer.class);
+
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = createDateTime("updatedAt", java.time.LocalDateTime.class);
+
+    public QProduct(String variable) {
+        this(Product.class, forVariable(variable), INITS);
+    }
+
+    public QProduct(Path<? extends Product> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QProduct(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QProduct(PathMetadata metadata, PathInits inits) {
+        this(Product.class, metadata, inits);
+    }
+
+    public QProduct(Class<? extends Product> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.category = inits.isInitialized("category") ? new QCategory(forProperty("category"), inits.get("category")) : null;
+    }
+
+}
+
