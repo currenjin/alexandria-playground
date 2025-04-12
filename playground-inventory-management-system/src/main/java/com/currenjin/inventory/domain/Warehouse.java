e4aeb18ad6eb7ab7648dea60d1a4001bc4130a26
currenjin <hyun0524e@naver.com>
2025-04-12 20:01:00 +0900

    InventoryManagementSystem: Apply entity for warehouse

--- playground-inventory-management-system/src/main/java/com/currenjin/inventory/domain/Warehouse.java
+++ playground-inventory-management-system/src/main/java/com/currenjin/inventory/domain/Warehouse.java
@@ -1,8 +1,24 @@
 package com.currenjin.inventory.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "warehouses")
 public class Warehouse {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false)
 	private String name;
+
+	@Column(name = "location", nullable = false)
 	private String location;
 
 	public Warehouse() {
