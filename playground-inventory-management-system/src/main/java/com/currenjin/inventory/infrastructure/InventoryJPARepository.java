77329479cc0b8462d48b3350c0ac97b7aa9a61cc
currenjin <hyun0524e@naver.com>
2025-04-12 20:02:56 +0900

    InventoryManagementSystem: Feat InventoryRepository

---
+++ playground-inventory-management-system/src/main/java/com/currenjin/inventory/infrastructure/InventoryJPARepository.java
@@ -0,0 +1,9 @@
+package com.currenjin.inventory.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.currenjin.inventory.domain.Inventory;
+import com.currenjin.inventory.domain.repository.InventoryRepository;
+
+public interface InventoryJPARepository extends InventoryRepository, JpaRepository<Inventory, Integer> {
+}
