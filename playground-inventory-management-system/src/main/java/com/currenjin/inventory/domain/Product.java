5bb08401d94968aceb68f2dafd13a5fef99b898e
currenjin <hyun0524e@naver.com>
2025-04-12 20:00:13 +0900

    InventoryManagementSystem: Apply entity for product

--- playground-inventory-management-system/src/main/java/com/currenjin/inventory/domain/Product.java
+++ playground-inventory-management-system/src/main/java/com/currenjin/inventory/domain/Product.java
@@ -2,10 +2,28 @@ package com.currenjin.inventory.domain;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false)
 	private String name;
+
+	@Column(name = "description", nullable = false)
 	private String description;
+
+	@Column(name = "price", nullable = false, columnDefinition = "BIGINT")
 	private BigDecimal price;
 
 	public Product() {
