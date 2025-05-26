08fdb3a167c03f2e691897dc55d476bb958644cf
currenjin <hyun0524e@naver.com>
2025-05-26 23:33:45 +0900

    ImplementationXUnit: Add failed test - wasRun

---
+++ implementation-x-unit/src/main/java/com/currenjin/WasRun.java
@@ -0,0 +1,9 @@
+package com.currenjin;
+
+public class WasRun {
+	public boolean wasRun;
+
+	public void testMethod() {
+
+	}
+}
--- implementation-x-unit/src/main/java/com/currenjin/XUnitTest.java
+++ implementation-x-unit/src/main/java/com/currenjin/XUnitTest.java
@@ -1,4 +1,10 @@
 package com.currenjin;
 
 public class XUnitTest {
+	public static void main(String[] args) {
+		WasRun wasRun = new WasRun();
+		System.out.println(wasRun.wasRun);
+		wasRun.testMethod();
+		System.out.println(wasRun.wasRun);
+	}
 }
