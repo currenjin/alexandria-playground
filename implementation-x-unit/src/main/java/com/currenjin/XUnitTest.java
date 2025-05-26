9a0747d17db4600d817a6e982d5b028bb43ef6d5
currenjin <hyun0524e@naver.com>
2025-05-27 00:23:13 +0900

    ImplementationXUnit: Refactor to static method for suite

--- implementation-x-unit/src/main/java/com/currenjin/XUnitTest.java
+++ implementation-x-unit/src/main/java/com/currenjin/XUnitTest.java
@@ -3,13 +3,7 @@ package com.currenjin;
 public class XUnitTest {
 	public static void main(String[] args) {
 		TestResult result = new TestResult();
-		TestSuite suite = new TestSuite();
-
-		suite.add(new TestCaseTest("testTemplateMethod"));
-		suite.add(new TestCaseTest("testResult"));
-		suite.add(new TestCaseTest("testFailedResultFormatting"));
-		suite.add(new TestCaseTest("testFailedResult"));
-		suite.add(new TestCaseTest("testSuite"));
+		TestSuite suite = TestCaseTest.suite();
 
 		suite.run(result);
 
