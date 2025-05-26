9c7c7c56e6ae9b5b156c2e4a9935d790722da424
currenjin <hyun0524e@naver.com>
2025-05-26 23:51:57 +0900

    ImplementationXUnit: Add setUp

--- implementation-x-unit/src/main/java/com/currenjin/XUnitTest.java
+++ implementation-x-unit/src/main/java/com/currenjin/XUnitTest.java
@@ -2,6 +2,7 @@ package com.currenjin;
 
 public class XUnitTest {
 	public static void main(String[] args) {
-		new TestCaseTest("testRun").testRun();
+		new TestCaseTest("testRun").run();
+		new TestCaseTest("testSetUp").run();
 	}
 }
