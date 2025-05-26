d1a9cdbd55cfa7503ae21fecb3a18844251b02b4
currenjin <hyun0524e@naver.com>
2025-05-27 00:08:16 +0900

    ImplementationXUnit: Add failedCount

--- implementation-x-unit/src/main/java/com/currenjin/TestResult.java
+++ implementation-x-unit/src/main/java/com/currenjin/TestResult.java
@@ -2,12 +2,17 @@ package com.currenjin;
 
 public class TestResult {
 	private int runCount = 0;
+	private int failedCount = 0;
 
 	public void testStarted() {
 		runCount++;
 	}
 
+	public void testFailed() {
+		failedCount++;
+	}
+
 	public String getSummary() {
-		return runCount + " run, 0 failed";
+		return runCount + " run, " + failedCount + " failed";
 	}
 }
