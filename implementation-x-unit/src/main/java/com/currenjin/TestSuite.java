a3897af2c2633f31912de4ed04232783fbbb3ca9
wm-currenjin <currenjin@wemeetmobility.com>
2025-05-27 12:20:58 +0900

    ImplementationXUnit: Refactor TestSuite for constructor

--- implementation-x-unit/src/main/java/com/currenjin/TestSuite.java
+++ implementation-x-unit/src/main/java/com/currenjin/TestSuite.java
@@ -1,17 +1,33 @@
 package com.currenjin;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestSuite implements Test {
 	List<Test> tests = new ArrayList<>();
+
+	public TestSuite(Class<? extends TestCase> testClass) {
+		Arrays.stream(testClass.getMethods())
+				.filter(method -> method.getName().startsWith("test"))
+				.forEach(method -> {
+                    try {
+						add(testClass.getConstructor(String.class).newInstance(method.getName()));
+                    } catch (Exception e) {
+						throw new RuntimeException(e);
+                    }
+                });
+	}
+
+	public TestSuite() {
+
+	}
 
 	public void add(Test test) {
 		tests.add(test);
 	}
 
 	public void run(TestResult result) {
-		tests.forEach(t -> {
-			t.run(result);
-		});
+		tests.forEach(t -> t.run(result));
 	}
 }
