dcdce5041e01ac13c6088fd54717968d548f163f
currenjin <hyun0524e@naver.com>
2025-07-13 20:28:46 +0900

    PlaygroundTracingOrderEvent: add subscriber id in subscriber and subscribe method for message queue

--- playground-tracing-order-event/src/main/kotlin/com/currenjin/MessageQueue.kt
+++ playground-tracing-order-event/src/main/kotlin/com/currenjin/MessageQueue.kt
@@ -5,10 +5,15 @@ import java.util.concurrent.ConcurrentLinkedQueue
 
 class MessageQueue {
     private val queue = ConcurrentLinkedQueue<OrderEvent>()
+    private val subscriberIds = mutableListOf<String>()
 
     fun push(event: OrderEvent) {
         queue.offer(event)
     }
 
     fun poll(): OrderEvent = queue.poll()
+
+    fun subscribe(subscriberId: String) {
+        subscriberIds.add(subscriberId)
+    }
 }
--- playground-tracing-order-event/src/main/kotlin/com/currenjin/Subscriber.kt
+++ playground-tracing-order-event/src/main/kotlin/com/currenjin/Subscriber.kt
@@ -5,11 +5,16 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.UUID
 
 class Subscriber(
     private val messageQueue: MessageQueue,
 ) {
+    private val subscriberId = UUID.randomUUID().toString()
+
     fun subscribe(callback: (OrderEvent) -> Unit) {
+        messageQueue.subscribe(subscriberId)
+
         CoroutineScope(Dispatchers.Default).launch {
             while (true) {
                 val event = messageQueue.poll()
