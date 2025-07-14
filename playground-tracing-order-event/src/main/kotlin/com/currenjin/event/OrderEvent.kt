1462cb1724e5b8712ac16b039779b9dedebd5155
currenjin <hyun0524e@naver.com>
2025-07-14 22:42:53 +0900

    PlaygroundTracingOrderEvent: add traceId in orderEvent

--- playground-tracing-order-event/src/main/kotlin/com/currenjin/event/OrderEvent.kt
+++ playground-tracing-order-event/src/main/kotlin/com/currenjin/event/OrderEvent.kt
@@ -1,10 +1,12 @@
 package com.currenjin.event
 
 import java.math.BigDecimal
+import java.util.UUID
 
 data class OrderEvent(
     val orderId: String,
     val userId: String,
     val amount: BigDecimal,
     val eventType: String,
+    val traceId: String = UUID.randomUUID().toString(),
 )
