4acccf0f5834671c3407ba5579935df77f3e35c2
currenjin <hyun0524e@naver.com>
2025-07-21 12:56:25 +0900

    PlaygroundTraceingOrderEvent: add message class

--- playground-tracing-order-event/src/main/kotlin/com/currenjin/Message.kt
+++ playground-tracing-order-event/src/main/kotlin/com/currenjin/Message.kt
@@ -1,3 +1,11 @@
 package com.currenjin
 
-open class Message
+import java.util.UUID
+
+data class Message(
+    val id: String = UUID.randomUUID().toString(),
+    val traceId: String = UUID.randomUUID().toString(),
+    val payload: Any,
+    val type: String,
+    val timestamp: Long = System.currentTimeMillis(),
+)
