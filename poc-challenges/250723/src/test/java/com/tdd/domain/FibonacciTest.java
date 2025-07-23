1f80f8de37502b6ccc70b6c0c6731d052cc7ba5e
currenjin <hyun0524e@naver.com>
2025-07-23 21:51:41 +0900

    Challenge: 250723 fibonacci

--- poc-challenges/250723/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250723/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -1,4 +1,14 @@
 package com.tdd.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class FibonacciTest {
+	@ParameterizedTest
+	@CsvSource(value = {"0, 0", "1, 1", "2, 1", "3, 2", "4, 3", "5, 5", "6, 8", "7, 13"})
+	void fibonacci_test(int n, int result) {
+		assertThat(Fibonacci.fib(n)).isEqualTo(result);
+	}
 }
