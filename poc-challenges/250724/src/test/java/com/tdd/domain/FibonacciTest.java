9c246cf27292d130e411b16ca529f148e5f323ce
currenjin <hyun0524e@naver.com>
2025-07-24 12:26:35 +0900

    Challenge: add passed test

--- poc-challenges/250724/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250724/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class FibonacciTest {
 	@Test
 	void fibonacci_test() {
-		int[][] numbers = {{0,0}, {1,1}, {1,2}, {2,3}, {3,4}, {5,5}};
+		int[][] numbers = {{0,0}, {1,1}, {1,2}, {2,3}, {3,4}, {5,5}, {8,6}, {13,7}, {21,8}};
 
 		for (int[] number : numbers) {
 			assertEquals(number[0], Fibonacci.fib(number[1]));
