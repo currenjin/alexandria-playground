94a86266aabf48cb6e8ad53971ca2d232bd001d5
currenjin <hyun0524e@naver.com>
2025-07-24 12:14:53 +0900

    Challenge: 0,0 test

--- poc-challenges/250724/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250724/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -1,10 +1,12 @@
 package com.tdd.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class FibonacciTest {
 	@Test
 	void fibonacci_test() {
-
+		assertEquals(0, Fibonacci.fib(0));
 	}
 }
