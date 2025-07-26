33c0650598e53fead14ffa846fec6b2f478339f1
currenjin <hyun0524e@naver.com>
2025-07-26 10:41:23 +0900

    Challenge 250726: add 3,4 failed test

--- poc-challenges/250726/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250726/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class FibonacciTest {
 	@ParameterizedTest
-	@CsvSource(value = {"0,0", "1,1", "1,2", "2,3"})
+	@CsvSource(value = {"0,0", "1,1", "1,2", "2,3", "3,4"})
 	void fibonacci(int result, int n) {
 		assertEquals(result, Fibonacci.fib(n));
 	}
