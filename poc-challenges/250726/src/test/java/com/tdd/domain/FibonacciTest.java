84ba3824ab07be4e4f6e5df2fb5ed26b4991a550
currenjin <hyun0524e@naver.com>
2025-07-26 10:40:39 +0900

    Challenge 250726: refactor 2,3 test 코드가 2-3번 반복된다 싶으면 반복을 제거해주면 된다.

--- poc-challenges/250726/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250726/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -2,14 +2,13 @@ package com.tdd.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class FibonacciTest {
-	@Test
-	void fibonacci() {
-		assertEquals(0, Fibonacci.fib(0));
-		assertEquals(1, Fibonacci.fib(1));
-		assertEquals(1, Fibonacci.fib(2));
-		assertEquals(2, Fibonacci.fib(3));
+	@ParameterizedTest
+	@CsvSource(value = {"0,0", "1,1", "1,2", "2,3"})
+	void fibonacci(int result, int n) {
+		assertEquals(result, Fibonacci.fib(n));
 	}
 }
