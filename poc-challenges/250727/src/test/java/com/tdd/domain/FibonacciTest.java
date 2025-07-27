01ef761f3f6f5d30f9ce46aaa87a92fc51a8e6e5
currenjin <hyun0524e@naver.com>
2025-07-27 09:14:17 +0900

    Challenge 250727: refactor test

--- poc-challenges/250727/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250727/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -7,9 +7,10 @@ import org.junit.jupiter.api.Test;
 public class FibonacciTest {
 	@Test
 	void fibonacci_test() {
-		assertEquals(0, Fibonacci.fib(0));
-		assertEquals(1, Fibonacci.fib(1));
-		assertEquals(1, Fibonacci.fib(2));
-		assertEquals(2, Fibonacci.fib(3));
+		int[][] numbers = {{0,0}, {1,1}, {1,2}, {2,3}};
+
+		for (int[] number : numbers) {
+			assertEquals(number[0], Fibonacci.fib(number[1]));
+		}
 	}
 }
