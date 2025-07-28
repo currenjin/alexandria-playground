dc7589e6513b90d140f5ba9c26a3203f290a1077
currenjin <hyun0524e@naver.com>
2025-07-28 21:48:08 +0900

    Challenge 250728: add 2,3 test

--- poc-challenges/250728/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250728/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FibonacciTest {
     @ParameterizedTest
-    @CsvSource(value = {"0,0", "1,1", "1,2"})
+    @CsvSource(value = {"0,0", "1,1", "1,2", "2,3"})
     void fibonacci_test(int result, int number) {
         assertEquals(result, Fibonacci.fib(number));
     }
