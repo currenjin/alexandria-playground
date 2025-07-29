3a61c7e229c8e6a1ab08254f094b48d931b86516
currenjin <hyun0524e@naver.com>
2025-07-29 21:36:10 +0900

    Challenge 250729 Factorial: init project

--- poc-challenges/250729/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250729/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -2,22 +2,19 @@ package com.tdd.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class FibonacciTest {
-	@Test
-	void fibonacci_test() {
-		assertEquals(0, fib(0));
-		assertEquals(1, fib(1));
-		assertEquals(1, fib(2));
-		assertEquals(2, fib(3));
-		assertEquals(3, fib(4));
-		assertEquals(5, fib(5));
+	// 0, 1, 1, 2, 3, 5, 8, 13, 21
+	@ParameterizedTest
+	@CsvSource(value = {"0,0", "1,1", "1,2", "2,3", "3,4", "5,5"})
+	void fibonacci_name(int result, int number) {
+		assertEquals(result, fib(number));
 	}
 
 	private int fib(int i) {
-		if (i == 0) return 0;
-		if (i <= 2) return 1;
+		if (i < 2) return i;
 		return fib(i - 1) + fib(i - 2);
 	}
 }
