95bec0a1ddb8235974d55f802f1756558bb6f1f9
currenjin <hyun0524e@naver.com>
2025-07-29 20:59:42 +0900

    Challenge 250729: add 3,4 test

--- poc-challenges/250729/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250729/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -11,11 +11,12 @@ public class FibonacciTest {
 		assertEquals(1, fib(1));
 		assertEquals(1, fib(2));
 		assertEquals(2, fib(3));
+		assertEquals(3, fib(4));
 	}
 
 	private int fib(int i) {
 		if (i == 0) return 0;
 		if (i <= 2) return 1;
-		return 2;
+		return fib(i - 1) + 1;
 	}
 }
