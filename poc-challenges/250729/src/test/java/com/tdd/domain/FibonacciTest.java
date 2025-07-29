7d3222f8b29951620a0a0683161065658c92ed1a
Hyunjin-Jeong <hyun0524e@naver.com>
2025-07-29 21:00:24 +0900

    Challenge/250729 (#83)
    * Challenge 250729: init project
    
    * Challenge 250729: add 0,0 1,1 test
    
    * Challenge 250729: add 1,2 test
    
    * Challenge 250729: add 2,3 test
    
    * Challenge 250729: add 3,4 test
    
    * Challenge 250729: add 5,5 test

---
+++ poc-challenges/250729/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -0,0 +1,23 @@
+package com.tdd.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FibonacciTest {
+	@Test
+	void fibonacci_test() {
+		assertEquals(0, fib(0));
+		assertEquals(1, fib(1));
+		assertEquals(1, fib(2));
+		assertEquals(2, fib(3));
+		assertEquals(3, fib(4));
+		assertEquals(5, fib(5));
+	}
+
+	private int fib(int i) {
+		if (i == 0) return 0;
+		if (i <= 2) return 1;
+		return fib(i - 1) + fib(i - 2);
+	}
+}
