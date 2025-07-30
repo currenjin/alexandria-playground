2b0cd328be339c835b356437f9f704a5f0fdd6f6
currenjin <hyun0524e@naver.com>
2025-07-30 11:13:54 +0900

    Challenge 250730: add 24,4 test

--- poc-challenges/250730/src/test/java/com/tdd/domain/FactorialTest.java
+++ poc-challenges/250730/src/test/java/com/tdd/domain/FactorialTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class FactorialTest {
 	@Test
 	void factorial_test() {
-		int[][] numbers = {{1,1}, {2,2}, {6,3}};
+		int[][] numbers = {{1,1}, {2,2}, {6,3}, {24,4}};
 
 		for (int[] number : numbers) {
 			assertEquals(number[0], Factorial.fac(number[1]));
@@ -17,7 +17,7 @@ public class FactorialTest {
 	private static class Factorial {
 		public static int fac(int i) {
 			if (i <= 2) return i;
-			return 6;
+			return fac(i - 1) * i;
 		}
 	}
 }
