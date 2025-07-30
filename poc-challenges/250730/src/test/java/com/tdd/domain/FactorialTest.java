7e140af17cfea966a51084783284f7f5e3077b0d
currenjin <hyun0524e@naver.com>
2025-07-30 11:11:26 +0900

    Challenge 250730: add 6,3 test

--- poc-challenges/250730/src/test/java/com/tdd/domain/FactorialTest.java
+++ poc-challenges/250730/src/test/java/com/tdd/domain/FactorialTest.java
@@ -9,11 +9,13 @@ public class FactorialTest {
 	void factorial_test() {
 		assertEquals(1, Factorial.fac(1));
 		assertEquals(2, Factorial.fac(2));
+		assertEquals(6, Factorial.fac(3));
 	}
 
 	private static class Factorial {
 		public static int fac(int i) {
-			return i;
+			if (i <= 2) return i;
+			return 6;
 		}
 	}
 }
