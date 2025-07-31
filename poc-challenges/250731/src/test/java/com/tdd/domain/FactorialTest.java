7eef3e5d7d04bdd1c75f25e3d75480e11d8a453a
currenjin <hyun0524e@naver.com>
2025-07-31 10:52:08 +0900

    Challenge 250731: refactor if statement for business logic

--- poc-challenges/250731/src/test/java/com/tdd/domain/FactorialTest.java
+++ poc-challenges/250731/src/test/java/com/tdd/domain/FactorialTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorialTest {
     @ParameterizedTest
-    @CsvSource(value = {"0,0", "1,1", "2,2", "6,3", "24,4"})
+    @CsvSource(value = {"0,0", "1,1", "2,2", "6,3", "24,4", "120,5"})
     void factorial_test(int result, int number) {
         assertEquals(result, fac(number));
     }
 
     private int fac(int n) {
-        if (n <= 2) return n;
+        if (n <= 1) return n;
         return fac(n - 1) * n;
     }
 }
