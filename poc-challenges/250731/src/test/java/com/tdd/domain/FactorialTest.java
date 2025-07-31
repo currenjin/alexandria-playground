5f2738fe9e751cab2c87947664e5c8460b2e52de
currenjin <hyun0524e@naver.com>
2025-07-31 10:51:26 +0900

    Challenge 250731: add 24,4 test

--- poc-challenges/250731/src/test/java/com/tdd/domain/FactorialTest.java
+++ poc-challenges/250731/src/test/java/com/tdd/domain/FactorialTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorialTest {
     @ParameterizedTest
-    @CsvSource(value = {"0,0", "1,1", "2,2", "6,3"})
+    @CsvSource(value = {"0,0", "1,1", "2,2", "6,3", "24,4"})
     void factorial_test(int result, int number) {
         assertEquals(result, fac(number));
     }
 
     private int fac(int n) {
         if (n <= 2) return n;
-        return 6;
+        return fac(n - 1) * n;
     }
 }
