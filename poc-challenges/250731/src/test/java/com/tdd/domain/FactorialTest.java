02ca2b9dd48ac29b808df297b8ef5e4f66f96e3f
currenjin <hyun0524e@naver.com>
2025-07-31 10:50:01 +0900

    Challenge 250731: refactor to parameterized test

--- poc-challenges/250731/src/test/java/com/tdd/domain/FactorialTest.java
+++ poc-challenges/250731/src/test/java/com/tdd/domain/FactorialTest.java
@@ -1,15 +1,15 @@
 package com.tdd.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorialTest {
-    @Test
-    void factorial_test() {
-        assertEquals(0, fac(0));
-        assertEquals(1, fac(1));
-        assertEquals(2, fac(2));
+    @ParameterizedTest
+    @CsvSource(value = {"0,0", "1,1", "2,2"})
+    void factorial_test(int result, int number) {
+        assertEquals(result, fac(number));
     }
 
     private int fac(int n) {
