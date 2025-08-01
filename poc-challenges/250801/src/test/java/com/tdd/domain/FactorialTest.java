8d91e4f9c6073d58cc8c57151d38dd4c3a5160b0
currenjin <hyun0524e@naver.com>
2025-08-01 12:46:19 +0900

    Challenge 250801: add 1,1 test

--- poc-challenges/250801/src/test/java/com/tdd/domain/FactorialTest.java
+++ poc-challenges/250801/src/test/java/com/tdd/domain/FactorialTest.java
@@ -2,8 +2,15 @@ package com.tdd.domain;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FactorialTest {
 	@Test
 	void factorial_test() {
+		assertEquals(1, fac(1));
+	}
+
+	private int fac(int i) {
+		return i;
 	}
 }
