0a1f469e44476c7b0a0c08624b15818e9393de94
Hyunjin-Jeong <hyun0524e@naver.com>
2025-08-01 13:33:17 +0900

    Challenge/250801 (#87)
    * Challenge 250801: init project
    
    * Challenge 250801: add 1,1 test
    
    * Challenge 250801: add 2,2 test
    
    * Challenge 250801: add 6,3 test
    
    * Challenge 250801: refactor to parameterized test
    
    * Challenge 250801: refactor business logic
    
    * Challenge 250801: refactor business logic 2
    
    * Challenge 250801: add 24,4 test

---
+++ poc-challenges/250801/src/test/java/com/tdd/domain/FactorialTest.java
@@ -0,0 +1,19 @@
+package com.tdd.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FactorialTest {
+	@ParameterizedTest
+	@CsvSource(value = {"1,1", "2,2", "6,3", "24,4"})
+	void factorial_test(int result, int number) {
+		assertEquals(result, fac(number));
+	}
+
+	private int fac(int i) {
+		if (i <= 2) return i;
+		return fac(i - 1) * i;
+	}
+}
