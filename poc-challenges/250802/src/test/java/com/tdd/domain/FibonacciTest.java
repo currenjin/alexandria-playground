62129137a5006b2c8dde9fe32097bfa26d676338
currenjin <hyun0524e@naver.com>
2025-08-02 14:05:26 +0900

    Challenge 250802: add 0,0/1,1 test

--- poc-challenges/250802/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250802/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -1,4 +1,17 @@
 package com.tdd.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class FibonacciTest {
+	@Test
+	void fibonacci_test() {
+		assertEquals(0, fib(0));
+		assertEquals(1, fib(1));
+	}
+
+	private int fib(int n) {
+		return n;
+	}
 }
