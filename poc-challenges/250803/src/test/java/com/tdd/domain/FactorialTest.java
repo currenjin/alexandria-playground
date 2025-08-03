8310b14b8013f2934a694126831edfd3ec0c6d8f
Hyunjin-Jeong <hyun0524e@naver.com>
2025-08-03 10:06:53 +0900

    Challenge/250803 (#89)
    * Challenge 250803: init project
    
    * Challenge 250803: add 1,1 failed test
    
    * Challenge 250803: add 1,1 passed test
    
    * Challenge 250803: add 2,2 passed test
    
    * Challenge 250803: refactor to parameterized test
    
    * Challenge 250803: add 6,3 passed test
    
    * Challenge 250803: refactor return for 6,3
    
    * Challenge 250803: refactor return for 6,3
    
    * Challenge 250803: add 24,4 passed test
    
    * Challenge 250803: add 120,5 failed test
    
    * Challenge 250803: add 120,5 passed test
    
    * Challenge 250803: refactor if statement

---
+++ poc-challenges/250803/src/test/java/com/tdd/domain/FactorialTest.java
@@ -0,0 +1,19 @@
+package com.tdd.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class FactorialTest {
+	@ParameterizedTest
+	@CsvSource(value = {"1,1", "2,2", "6,3", "24,4", "120,5"})
+	void factorial_test(int result, int number) {
+		assertEquals(result, fac(number));
+	}
+
+	private int fac(int n) {
+		if (n == 1) return n;
+		return fac(n - 1) * n;
+	}
+}
