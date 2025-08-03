e73d2b06e87c6f7408805a2ffea2db061f1423e6
currenjin <hyun0524e@naver.com>
2025-08-03 10:02:47 +0900

    Challenge 250803: add 6,3 passed test

--- poc-challenges/250803/src/test/java/com/tdd/domain/FactorialTest.java
+++ poc-challenges/250803/src/test/java/com/tdd/domain/FactorialTest.java
@@ -7,12 +7,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class FactorialTest {
 	@ParameterizedTest
-	@CsvSource(value = {"1,1", "2,2"})
+	@CsvSource(value = {"1,1", "2,2", "6,3"})
 	void factorial_test(int result, int number) {
 		assertEquals(result, fac(number));
 	}
 
 	private int fac(int n) {
-		return n;
+		if (n <= 2) return n;
+		return 6;
 	}
 }
