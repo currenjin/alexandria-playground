21c7724f8eaa9ccdca7e412023d92c9cc2c6d33e
currenjin <hyun0524e@naver.com>
2025-08-04 08:01:44 +0900

    Challenge 250804: refactor if statement

--- poc-challenges/250804/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250804/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -13,8 +13,8 @@ public class FibonacciTest {
 	}
 
 	private int fib(int n) {
-		if (n <= 1) return n;
-		if (n == 2) return 1;
+		if (n == 0) return 0;
+		if (n <= 2) return 1;
 		return 2;
 	}
 }
