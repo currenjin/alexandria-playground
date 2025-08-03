507b48009e51dbf3ee6579013827f51fe0bfe6ec
currenjin <hyun0524e@naver.com>
2025-08-03 10:00:57 +0900

    Challenge 250803: add 1,1 failed test

--- poc-challenges/250803/src/test/java/com/tdd/domain/FactorialTest.java
+++ poc-challenges/250803/src/test/java/com/tdd/domain/FactorialTest.java
@@ -1,5 +1,16 @@
 package com.tdd.domain;
 
-public class FactorialTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+public class FactorialTest {
+	@Test
+	void factorial_test() {
+		assertEquals(1, fac(1));
+	}
+
+	private int fac(int n) {
+		return 0;
+	}
 }
