bb10e07cf1c9f0e5ba7584f62372fc51acf86e44
currenjin <hyun0524e@naver.com>
2025-08-04 08:04:29 +0900

    Challenge 250804: add passed test

--- poc-challenges/250804/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250804/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class FibonacciTest {
 	@ParameterizedTest
-	@CsvSource({"0,0", "1,1", "1,2", "2,3", "3,4", "5,5"})
+	@CsvSource({"0,0", "1,1", "1,2", "2,3", "3,4", "5,5", "8,6", "13,7"})
 	void fibonacci(int result, int number) {
 		assertEquals(result, fib(number));
 	}
