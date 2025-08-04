bc9156673aecb3d380b70df76e2c8b485e0fef25
Hyunjin-Jeong <hyun0524e@naver.com>
2025-08-05 08:18:06 +0900

    Challenge/250805 (#91)
    * Challenge 250805: init project
    
    * Challenge 250805: add 0,0 passed test
    
    * Challenge 250805: add 1,1 failed test
    
    * Challenge 250805: add 1,1 passed test
    
    * Challenge 250805: refactor to for statement test
    
    * Challenge 250805: add 1,2 failed test
    
    * Challenge 250805: add 1,2 passed test
    
    * Challenge 250805: add 2,3 failed test
    
    * Challenge 250805: add 2,3 passed test
    
    * Challenge 250805: add 3,4 passed test
    
    * Challenge 250805: add 5,5 failed test
    
    * Challenge 250805: add 5,5 passed test
    
    * Challenge 250805: refactor to if statement
    
    * Challenge 250805: add passed tests

---
+++ poc-challenges/250805/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -0,0 +1,16 @@
+package com.tdd.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FibonacciTest {
+	@Test
+	void fibonacci() {
+		int[][] numbers = {{0,0}, {1,1}, {1,2}, {2,3}, {3,4}, {5,5}, {8,6}, {13,7}};
+
+		for (int[] number : numbers) {
+			assertEquals(number[0], Fibonacci.fib(number[1]));
+		}
+	}
+}
