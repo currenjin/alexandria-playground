5f0f18230cb859ff9a52503fd789daa7a3d88150
currenjin <hyun0524e@naver.com>
2025-08-05 08:05:29 +0900

    Challenge 250805: add 0,0 passed test

--- poc-challenges/250805/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250805/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -1,4 +1,12 @@
 package com.tdd.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class FibonacciTest {
+	@Test
+	void fibonacci() {
+		assertEquals(0, Fibonacci.fib(0));
+	}
 }
