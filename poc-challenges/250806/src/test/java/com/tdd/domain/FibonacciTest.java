f9e8addeaa086f6dd7e6d4f00d5b619cd93b9dfd
currenjin <hyun0524e@naver.com>
2025-08-06 21:22:07 +0900

    Challenge 250806: add 1,2 failed test

--- poc-challenges/250806/src/test/java/com/tdd/domain/FibonacciTest.java
+++ poc-challenges/250806/src/test/java/com/tdd/domain/FibonacciTest.java
@@ -9,5 +9,6 @@ public class FibonacciTest {
     void fibonacci() {
         assertEquals(0, Fibonacci.fib(0));
         assertEquals(1, Fibonacci.fib(1));
+        assertEquals(1, Fibonacci.fib(2));
     }
 }
