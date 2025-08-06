23d22990a4f5052cd9ee071e6d4c2aacb2bf1650
currenjin <hyun0524e@naver.com>
2025-08-06 21:24:49 +0900

    Challenge 250806: add 2,3 passed test

--- poc-challenges/250806/src/test/java/com/tdd/domain/Fibonacci.java
+++ poc-challenges/250806/src/test/java/com/tdd/domain/Fibonacci.java
@@ -3,6 +3,7 @@ package com.tdd.domain;
 public class Fibonacci {
     public static int fib(int i) {
         if (i == 0) return 0;
-        return 1;
+        if (i <= 2) return 1;
+        return 2;
     }
 }
