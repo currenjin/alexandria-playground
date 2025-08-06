95f5cd68871893047dbf67e662a29a7befaf6527
currenjin <hyun0524e@naver.com>
2025-08-06 21:28:11 +0900

    Challenge 250806: refactor business logic

--- poc-challenges/250806/src/test/java/com/tdd/domain/Fibonacci.java
+++ poc-challenges/250806/src/test/java/com/tdd/domain/Fibonacci.java
@@ -2,8 +2,7 @@ package com.tdd.domain;
 
 public class Fibonacci {
     public static int fib(int i) {
-        if (i == 0) return 0;
-        if (i <= 2) return 1;
+        if (i <= 1) return i;
         return fib(i - 1) + fib(i - 2);
     }
 }
