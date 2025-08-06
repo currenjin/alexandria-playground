cffd9a7a1102b36e68f7606763bd7393d473bdc5
Hyunjin-Jeong <hyun0524e@naver.com>
2025-08-07 08:21:37 +0900

    Challenge/250807 (#93)
    * init: project
    
    * feat: push then top returns same value
    
    * feat: top throws exception when stack is empty
    
    * feat: push multiple then getMin returns minimum value
    
    * feat: pop then getMin changes correctly

---
+++ poc-challenges/250807/src/test/java/com/tdd/domain/MinStack.java
@@ -0,0 +1,41 @@
+package com.tdd.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class MinStack {
+	private List<Integer> data = new ArrayList<>();
+	private List<Integer> minStack = new ArrayList<>();
+
+	public void push(int n) {
+		data.add(n);
+		int min = minStack.isEmpty()
+			? n
+			: Math.min(n, minStack.get(minStack.size() - 1));
+		minStack.add(min);
+	}
+
+	public int top() {
+		if (data.isEmpty()) {
+			throw new NoSuchElementException("stack is empty");
+		}
+
+		return data.get(data.size() - 1);
+	}
+
+	public int getMin() {
+		if (minStack.isEmpty()) {
+			throw new NoSuchElementException("stack is empty");
+		}
+		return minStack.get(minStack.size() - 1);
+	}
+
+	public void pop() {
+		if (minStack.isEmpty()) {
+			throw new NoSuchElementException("stack is empty");
+		}
+		data.remove(data.size() - 1);
+		minStack.remove(minStack.size() - 1);
+	}
+}
---
+++ poc-challenges/250807/src/test/java/com/tdd/domain/MinStackTest.java
@@ -0,0 +1,50 @@
+package com.tdd.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+
+public class MinStackTest {
+	@Test
+	void push_then_top_returnsSameValue() {
+		MinStack stack = new MinStack();
+
+		stack.push(5);
+
+		assertEquals(5, stack.top());
+	}
+
+	@Test
+	void top_throwsException_whenStackIsEmpty() {
+		MinStack stack = new MinStack();
+
+		assertThrows(NoSuchElementException.class, stack::top);
+	}
+
+	@Test
+	void push_multiple_then_getMin_returnsMinimumValue() {
+		MinStack stack = new MinStack();
+
+		stack.push(5);
+		stack.push(10);
+		stack.push(3);
+
+		assertEquals(3, stack.getMin());
+	}
+
+	@Test
+	void pop_then_getMin_changesCorrectly() {
+		MinStack stack = new MinStack();
+
+		stack.push(5);
+		stack.push(1);
+		stack.push(3);
+		stack.pop();
+
+		assertEquals(1, stack.getMin());
+	}
+}
