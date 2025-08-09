14fe1871e2f29ba6c8f219fbf23c1aa2bd255485
currenjin <hyun0524e@naver.com>
2025-08-09 15:59:35 +0900

    feat: push_then_top_returnsPushedValue

--- poc-challenges/250809/src/test/java/com/tdd/domain/MinStackTest.java
+++ poc-challenges/250809/src/test/java/com/tdd/domain/MinStackTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.NoSuchElementException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MinStackTest {
@@ -12,5 +13,14 @@ public class MinStackTest {
 		MinStack stack = new MinStack();
 
 		assertThrows(NoSuchElementException.class, stack::top);
+	}
+
+	@Test
+	void push_then_top_returnsPushedValue() {
+		MinStack stack = new MinStack();
+
+		stack.push(1);
+
+		assertEquals(1, stack.top());
 	}
 }
