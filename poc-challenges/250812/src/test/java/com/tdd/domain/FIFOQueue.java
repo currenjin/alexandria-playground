399d696df375e087ed1afee944563d1fff9a2172
currenjin <hyun0524e@naver.com>
2025-08-12 08:00:46 +0900

    feat: enqueue_then_isEmpty_returnsFalse

--- poc-challenges/250812/src/test/java/com/tdd/domain/FIFOQueue.java
+++ poc-challenges/250812/src/test/java/com/tdd/domain/FIFOQueue.java
@@ -1,7 +1,17 @@
 package com.tdd.domain;
 
+import java.awt.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class FIFOQueue {
+	private final Deque<Integer> stack = new ArrayDeque<>();
+
 	public boolean isEmpty() {
-		return true;
+		return stack.isEmpty();
+	}
+
+	public void enqueue(int i) {
+		stack.push(i);
 	}
 }
