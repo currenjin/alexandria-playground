d8ba54e27f0b2712da07b161c74d6ef6b4529140
currenjin <hyun0524e@naver.com>
2025-08-13 07:45:28 +0900

    feat: enqueue_then_isEmpty_returnsFalse

--- poc-challenges/250813/src/test/java/com/tdd/domain/FIFOQueueTest.java
+++ poc-challenges/250813/src/test/java/com/tdd/domain/FIFOQueueTest.java
@@ -1,6 +1,9 @@
 package com.tdd.domain;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javax.swing.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +13,14 @@ public class FIFOQueueTest {
 		FIFOQueue queue = new FIFOQueue();
 
 		assertTrue(queue.isEmpty());
+	}
+
+	@Test
+	void enqueue_then_isEmpty_returnsFalse() {
+		FIFOQueue queue = new FIFOQueue();
+
+		queue.enqueue(1);
+
+		assertFalse(queue.isEmpty());
 	}
 }
