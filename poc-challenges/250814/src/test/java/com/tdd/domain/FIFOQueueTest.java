86f0b470b35bc6b397cd6e222a450725c3b8d0ba
Hyunjin-Jeong <hyun0524e@naver.com>
2025-08-14 08:47:29 +0900

    Challenge/250814 (#100)
    * init: project
    
    * feat: isEmpty_returnsTrue
    
    * feat: enqueue_then_isEmpty_returnsFalse
    
    * feat: peek_throwsException_whenEmpty
    
    * feat: enqueue_then_peek_returnsValue
    
    * feat: enqueue_multipleValues_then_peek_returnsFirstValue
    
    * feat: dequeue_throwsException_whenEmpty
    
    * feat: enqueue_then_dequeue_returnsValue
    
    * feat: enqueue_multipleValues_then_dequeue_returnsFirstValue
    
    * refactor: extract to validateEmptyQueue
    
    * feat: enqueue_multipleValues_then_dequeue_returnsValueInOrder
    
    * feat: interleaved_operations_behave_consistently

---
+++ poc-challenges/250814/src/test/java/com/tdd/domain/FIFOQueueTest.java
@@ -0,0 +1,109 @@
+package com.tdd.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FIFOQueueTest {
+	@Test
+	void isEmpty_returnsTrue() {
+		FIFOQueue queue = new FIFOQueue();
+
+		assertTrue(queue.isEmpty());
+	}
+
+	@Test
+	void enqueue_then_isEmpty_returnsFalse() {
+		FIFOQueue queue = new FIFOQueue();
+
+		queue.enqueue(1);
+
+		assertFalse(queue.isEmpty());
+	}
+
+	@Test
+	void peek_throwsException_whenEmpty() {
+		FIFOQueue queue = new FIFOQueue();
+
+		assertThrows(NoSuchElementException.class, queue::peek);
+	}
+
+	@Test
+	void enqueue_then_peek_returnsValue() {
+		FIFOQueue queue = new FIFOQueue();
+
+		queue.enqueue(1);
+
+		assertEquals(1, queue.peek());
+	}
+
+	@Test
+	void enqueue_multipleValues_then_peek_returnsFirstValue() {
+		FIFOQueue queue = new FIFOQueue();
+
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+
+		assertEquals(1, queue.peek());
+	}
+
+	@Test
+	void dequeue_throwsException_whenEmpty() {
+		FIFOQueue queue = new FIFOQueue();
+
+		assertThrows(NoSuchElementException.class, queue::dequeue);
+	}
+
+	@Test
+	void enqueue_then_dequeue_returnsValue() {
+		FIFOQueue queue = new FIFOQueue();
+
+		queue.enqueue(1);
+
+		assertEquals(1, queue.dequeue());
+	}
+
+	@Test
+	void enqueue_multipleValues_then_dequeue_returnsFirstValue() {
+		FIFOQueue queue = new FIFOQueue();
+
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+
+		assertEquals(1, queue.dequeue());
+	}
+
+	@Test
+	void enqueue_multipleValues_then_dequeue_returnsValueInOrder() {
+		FIFOQueue queue = new FIFOQueue();
+
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+
+		assertEquals(1, queue.dequeue());
+		assertEquals(2, queue.dequeue());
+		assertEquals(3, queue.dequeue());
+	}
+
+	@Test
+	void interleaved_operations_behave_consistently() {
+		FIFOQueue queue = new FIFOQueue();
+
+		queue.enqueue(1);
+		queue.enqueue(2);
+		assertEquals(1, queue.dequeue());
+
+		queue.enqueue(3);
+		assertEquals(2, queue.peek());
+		assertEquals(2, queue.dequeue());
+		assertEquals(3, queue.dequeue());
+
+		assertTrue(queue.isEmpty());
+	}
+}
