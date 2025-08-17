0de66e77ad6ea7f5ae0a7515c8122a1b07020945
currenjin <hyun0524e@naver.com>
2025-08-18 08:34:11 +0900

    feat: enqueue_then_isEmpty_returnsFalse

--- poc-challenges/250818/src/test/java/com/tdd/domain/CircularQueue.java
+++ poc-challenges/250818/src/test/java/com/tdd/domain/CircularQueue.java
@@ -2,11 +2,17 @@ package com.tdd.domain;
 
 public class CircularQueue {
 
+	private Integer single;
+
 	public CircularQueue(int i) {
 
 	}
 
 	public boolean isEmpty() {
-		return true;
+		return single == null;
+	}
+
+	public void enqueue(int i) {
+		single = i;
 	}
 }
--- poc-challenges/250818/src/test/java/com/tdd/domain/CircularQueueTest.java
+++ poc-challenges/250818/src/test/java/com/tdd/domain/CircularQueueTest.java
@@ -3,6 +3,8 @@ package com.tdd.domain;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import javax.management.openmbean.TabularDataSupport;
+
 import org.junit.jupiter.api.Test;
 
 public class CircularQueueTest {
@@ -11,5 +13,14 @@ public class CircularQueueTest {
 		CircularQueue queue = new CircularQueue(3);
 
 		assertTrue(queue.isEmpty());
+	}
+
+	@Test
+	void enqueue_then_isEmpty_returnsFalse() {
+		CircularQueue queue = new CircularQueue(3);
+
+		queue.enqueue(1);
+
+		assertFalse(queue.isEmpty());
 	}
 }
