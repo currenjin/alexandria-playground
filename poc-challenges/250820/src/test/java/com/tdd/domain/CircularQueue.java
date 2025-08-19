1630adbded685c9d19bffaa4c647364ed997f508
Hyunjin-Jeong <hyun0524e@naver.com>
2025-08-20 08:05:15 +0900

    Challenge/250820 (#108)
    * init: project
    
    * feat: isEmpty_returnsTrue_whenEmpty
    
    * feat: enqueue_thenIsEmpty_returnsFalse
    
    * rename: single to data
    
    * feat: peek_throwsException_whenEmpty
    
    * feat: enqueue_then_peek_returnsValue
    
    * feat: enqueue_multipleValues_then_peek_returnsFirstValue
    
    * feat: isFull_returnsFalse_whenEmpty
    
    * feat: enqueue_then_isFull_returnsTrue_whenFull
    
    * feat: dequeue_throwsException_whenEmpty
    
    * refactor: extract to validateEmptyQueue
    
    * feat: enqueue_then_dequeue_returnsFirstValue
    
    * feat: enqueue_multipleValues_then_dequeue_returnsValuesInOrder
    
    * feat: integration
    
    * feat: enqueue_throwsException_whenFull
    
    * refactor: extract to validateFullQueue

---
+++ poc-challenges/250820/src/test/java/com/tdd/domain/CircularQueue.java
@@ -0,0 +1,59 @@
+package com.tdd.domain;
+
+import java.util.NoSuchElementException;
+
+public class CircularQueue {
+
+	private final int capacity;
+	private final int[] data;
+	private int size = 0, tail = 0, head = 0;
+
+	public CircularQueue(int i) {
+		capacity = i;
+		data = new int[capacity];
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public void enqueue(int i) {
+		validateFullQueue();
+
+		data[tail] = i;
+		tail = (tail + 1) % capacity;
+		size++;
+	}
+
+	public int peek() {
+		validateEmptyQueue();
+
+		return data[head];
+	}
+
+	public boolean isFull() {
+		return size == capacity;
+	}
+
+	public int dequeue() {
+		validateEmptyQueue();
+
+		int value = data[head];
+		head = (head + 1) % capacity;
+		size--;
+
+		return value;
+	}
+
+	private void validateFullQueue() {
+		if (isFull()) {
+			throw new IllegalStateException("queue is full");
+		}
+	}
+
+	private void validateEmptyQueue() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("queue is empty");
+		}
+	}
+}
---
+++ poc-challenges/250820/src/test/java/com/tdd/domain/CircularQueueTest.java
@@ -0,0 +1,133 @@
+package com.tdd.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+
+import com.sun.org.apache.xerces.internal.util.AugmentationsImpl;
+
+public class CircularQueueTest {
+	@Test
+	void isEmpty_returnsTrue_whenEmpty() {
+		CircularQueue queue = new CircularQueue(1);
+
+		assertTrue(queue.isEmpty());
+	}
+
+	@Test
+	void enqueue_thenIsEmpty_returnsFalse() {
+		CircularQueue queue = new CircularQueue(1);
+
+		queue.enqueue(1);
+
+		assertFalse(queue.isEmpty());
+	}
+
+	@Test
+	void peek_throwsException_whenEmpty() {
+		CircularQueue queue = new CircularQueue(1);
+
+		assertThrows(NoSuchElementException.class, queue::peek);
+	}
+
+	@Test
+	void enqueue_then_peek_returnsValue() {
+		CircularQueue queue = new CircularQueue(1);
+
+		queue.enqueue(1);
+
+		assertEquals(1, queue.peek());
+	}
+
+	@Test
+	void enqueue_multipleValues_then_peek_returnsFirstValue() {
+		CircularQueue queue = new CircularQueue(3);
+
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+
+		assertEquals(1, queue.peek());
+	}
+
+	@Test
+	void isFull_returnsFalse_whenEmpty() {
+		CircularQueue queue = new CircularQueue(1);
+
+		assertFalse(queue.isFull());
+	}
+
+	@Test
+	void enqueue_then_isFull_returnsTrue_whenFull() {
+		CircularQueue queue = new CircularQueue(3);
+
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+
+		assertTrue(queue.isFull());
+	}
+
+	@Test
+	void dequeue_throwsException_whenEmpty() {
+		CircularQueue queue = new CircularQueue(1);
+
+		assertThrows(NoSuchElementException.class, queue::dequeue);
+	}
+
+	@Test
+	void enqueue_then_dequeue_returnsFirstValue() {
+		CircularQueue queue = new CircularQueue(1);
+
+		queue.enqueue(1);
+
+		assertEquals(1, queue.dequeue());
+	}
+
+	@Test
+	void enqueue_multipleValues_then_dequeue_returnsValuesInOrder() {
+		CircularQueue queue = new CircularQueue(3);
+
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+
+		assertEquals(1, queue.dequeue());
+		assertEquals(2, queue.dequeue());
+		assertEquals(3, queue.dequeue());
+	}
+
+	@Test
+	void enqueue_throwsException_whenFull() {
+		CircularQueue queue = new CircularQueue(2);
+
+		queue.enqueue(1);
+		queue.enqueue(2);
+
+		assertThrows(IllegalStateException.class, () -> queue.enqueue(3));
+	}
+
+	@Test
+	void integration() {
+		CircularQueue queue = new CircularQueue(3);
+
+		queue.enqueue(1);
+		assertEquals(1, queue.peek());
+		assertEquals(1, queue.dequeue());
+
+		queue.enqueue(2);
+		queue.enqueue(3);
+		queue.enqueue(4);
+		assertTrue(queue.isFull());
+		assertEquals(2, queue.dequeue());
+		assertEquals(3, queue.dequeue());
+		assertEquals(4, queue.peek());
+		assertEquals(4, queue.dequeue());
+		assertTrue(queue.isEmpty());
+	}
+}
