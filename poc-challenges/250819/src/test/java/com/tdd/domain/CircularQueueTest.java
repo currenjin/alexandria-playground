7de5aa1a43723d1bc7307d6caf07a68590f78924
currenjin <hyun0524e@naver.com>
2025-08-19 12:54:06 +0900

    feat: enqueue_then_isEmpty_returnFalse

--- poc-challenges/250819/src/test/java/com/tdd/domain/CircularQueueTest.java
+++ poc-challenges/250819/src/test/java/com/tdd/domain/CircularQueueTest.java
@@ -2,6 +2,9 @@ package com.tdd.domain;
 
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CircularQueueTest {
@@ -11,5 +14,14 @@ public class CircularQueueTest {
         CircularQueue queue = new CircularQueue(1);
 
         assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    void enqueue_then_isEmpty_returnFalse() {
+        CircularQueue queue = new CircularQueue(1);
+
+        queue.enqueue(1);
+
+        assertFalse(queue.isEmpty());
     }
 }
