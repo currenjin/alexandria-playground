f0137dabe1703fca2a34c3878fc1085938b1bb2c
currenjin <hyun0524e@naver.com>
2025-08-20 07:13:28 +0900

    feat: peek_throwsException_whenEmpty

--- poc-challenges/250820/src/test/java/com/tdd/domain/CircularQueueTest.java
+++ poc-challenges/250820/src/test/java/com/tdd/domain/CircularQueueTest.java
@@ -1,7 +1,10 @@
 package com.tdd.domain;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.NoSuchElementException;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +23,12 @@ public class CircularQueueTest {
 		queue.enqueue(1);
 
 		assertFalse(queue.isEmpty());
+	}
+
+	@Test
+	void peek_throwsException_whenEmpty() {
+		CircularQueue queue = new CircularQueue(1);
+
+		assertThrows(NoSuchElementException.class, queue::peek);
 	}
 }
