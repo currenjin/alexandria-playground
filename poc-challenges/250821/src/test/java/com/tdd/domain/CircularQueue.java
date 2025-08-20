6c1e63ab4b57d26aa3fc7931127e2a81eff9ca43
currenjin <hyun0524e@naver.com>
2025-08-21 08:19:33 +0900

    feat: enqueue_then_isEmpty_returnsFalse

--- poc-challenges/250821/src/test/java/com/tdd/domain/CircularQueue.java
+++ poc-challenges/250821/src/test/java/com/tdd/domain/CircularQueue.java
@@ -1,11 +1,17 @@
 package com.tdd.domain;
 
 public class CircularQueue {
+	private Integer data;
+
 	public CircularQueue(int i) {
 
 	}
 
 	public boolean isEmpty() {
-		return true;
+		return data == null;
+	}
+
+	public void enqueue(int i) {
+		data = i;
 	}
 }
