2b3fdf9d78c47ab32e7af18f5d823cf4c83749ae
currenjin <hyun0524e@naver.com>
2025-08-20 18:51:37 +0900

    feat: create throws exception when empty or negative fields

--- poc-table-custom-header/src/main/kotlin/com/currenjin/header/template/HeaderTemplate.kt
+++ poc-table-custom-header/src/main/kotlin/com/currenjin/header/template/HeaderTemplate.kt
@@ -4,4 +4,10 @@ data class HeaderTemplate(
     val tableName: String,
     val columnName: String,
     val sequence: Int,
-)
+) {
+    init {
+        require(tableName.isNotBlank()) { "tableName must not be blank" }
+        require(columnName.isNotBlank()) { "columnName must not be blank" }
+        require(sequence > 0) { "sequence must be greater than 0" }
+    }
+}
--- poc-table-custom-header/src/test/kotlin/com/currenjin/header/template/HeaderTemplateTest.kt
+++ poc-table-custom-header/src/test/kotlin/com/currenjin/header/template/HeaderTemplateTest.kt
@@ -1,6 +1,7 @@
 package com.currenjin.header.template
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertEquals
 
 class HeaderTemplateTest {
@@ -16,5 +17,38 @@ class HeaderTemplateTest {
         assertEquals("지급운임", template.columnName)
         assertEquals("오더관리", template.tableName)
         assertEquals(1, template.sequence)
+    }
+
+    @Test
+    fun create_throwsException_whenEmptyOrBlankTableName() {
+        assertThrows<IllegalArgumentException> {
+            HeaderTemplate(
+                tableName = "",
+                columnName = "지급운임",
+                sequence = 1,
+            )
+        }
+    }
+
+    @Test
+    fun create_throwsException_whenEmptyOrBlankColumnName() {
+        assertThrows<IllegalArgumentException> {
+            HeaderTemplate(
+                tableName = "오더관리",
+                columnName = "",
+                sequence = 1,
+            )
+        }
+    }
+
+    @Test
+    fun create_throwsException_whenZeroSequence() {
+        assertThrows<IllegalArgumentException> {
+            HeaderTemplate(
+                tableName = "오더관리",
+                columnName = "지급운임",
+                sequence = 0,
+            )
+        }
     }
 }
