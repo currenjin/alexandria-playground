74cd95816a00c6b1098d99f387157a4c6779beba
Hyunjin-Jeong <hyun0524e@naver.com>
2025-08-21 08:44:40 +0900

    Challenge/250821 (#109)
    * init: project
    
    * feat: isEmpty_returnsTrue
    
    * feat: enqueue_then_isEmpty_returnsFalse
    
    * feat: peek_throwsException_whenEmpty
    
    * feat: enqueue_then_peek_returnsValue
    
    * feat: enqueue_multipleValues_then_peek_returnsFirstValue
    
    * feat: isFull_returnsFalse
    
    * feat: isFull_returnsTrue_whenFull
    
    * feat: enqueue_throwsException_whenFull
    
    * feat: dequeue_throwsException_whenEmpty
    
    * feat: enqueue_then_dequeue_returnsValue
    
    * feat: enqueue_multipleValues_then_dequeue_returnsLastValue
    
    * feat: enqueue_multipleValues_then_dequeue_returnsLastValueInOrder
    
    * fix: dequeue
    
    * refactor: extract to validate empty queue
    
    * refactor: to circular queue
    
    * feat: wrap_around

---
+++ poc-challenges/250821/src/test/java/com/tdd/domain/CircularQueue.java
@@ -0,0 +1,54 @@
+package com.tdd.domain;
+
+import java.util.NoSuchElementException;
+
+public class CircularQueue {
+	private final int[] data;
+	private final int capacity;
+	private int head = 0, tail = 0, size = 0;
+
+	public CircularQueue(int i) {
+		capacity = i;
+		data = new int[capacity];
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public void enqueue(int i) {
+		if (isFull()) {
+			throw new IllegalStateException("queue is full");
+		}
+
+		data[tail] = i;
+		tail = (tail + 1) % capacity;
+		size++;
+	}
+
+	public int peek() {
+		validateEmptyQueue();
+
+		return data[head];
+	}
+
+	public boolean isFull() {
+		return size == capacity;
+	}
+
+	public int dequeue() {
+		validateEmptyQueue();
+
+		int value = data[head];
+		head = (head + 1) % capacity;
+		size--;
+
+		return value;
+	}
+
+	private void validateEmptyQueue() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("queue is empty");
+		}
+	}
+}
