8c86ec4bed4e790d41b2829fce56a77c9e77f438
currenjin <hyun0524e@naver.com>
2025-08-21 08:31:03 +0900

    feat: enqueue_multipleValues_then_dequeue_returnsLastValue

--- poc-challenges/250821/src/test/java/com/tdd/domain/CircularQueueTest.java
+++ poc-challenges/250821/src/test/java/com/tdd/domain/CircularQueueTest.java
@@ -98,4 +98,15 @@ public class CircularQueueTest {
 
 		assertEquals(1, queue.dequeue());
 	}
+
+	@Test
+	void enqueue_multipleValues_then_dequeue_returnsLastValue() {
+		CircularQueue queue = new CircularQueue(3);
+
+		queue.enqueue(1);
+		queue.enqueue(2);
+		queue.enqueue(3);
+
+		assertEquals(3, queue.dequeue());
+	}
 }
