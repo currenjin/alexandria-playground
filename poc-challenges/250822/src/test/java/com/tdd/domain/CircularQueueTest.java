9b69be24aa3751ca855f6452f067f2e1c1f774aa
currenjin <hyun0524e@naver.com>
2025-08-22 12:30:17 +0900

    feat: isEmpty_returnsTrue

--- poc-challenges/250822/src/test/java/com/tdd/domain/CircularQueueTest.java
+++ poc-challenges/250822/src/test/java/com/tdd/domain/CircularQueueTest.java
@@ -1,4 +1,14 @@
 package com.tdd.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CircularQueueTest {
+    @Test
+    void isEmpty_returnsTrue() {
+        CircularQueue queue = new CircularQueue(1);
+
+        assertTrue(queue.isEmpty());
+    }
 }
