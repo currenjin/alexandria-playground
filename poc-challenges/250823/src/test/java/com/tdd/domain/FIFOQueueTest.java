810a21cfe883590b22dda7cd256e351f3a19c9fb
currenjin <hyun0524e@naver.com>
2025-08-23 11:14:08 +0900

    feat: isEmpty_returnsTrue

--- poc-challenges/250823/src/test/java/com/tdd/domain/FIFOQueueTest.java
+++ poc-challenges/250823/src/test/java/com/tdd/domain/FIFOQueueTest.java
@@ -1,4 +1,14 @@
 package com.tdd.domain;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class FIFOQueueTest {
+	@Test
+	void isEmpty_returnsTrue() {
+		FIFOQueue queue = new FIFOQueue();
+
+		assertTrue(queue.isEmpty());
+	}
 }
