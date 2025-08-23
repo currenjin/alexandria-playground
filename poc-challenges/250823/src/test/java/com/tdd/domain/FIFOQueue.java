415b3838378648b15255092a22939188bcaab8c6
Hyunjin-Jeong <hyun0524e@naver.com>
2025-08-23 11:31:39 +0900

    Challenge/250823 (#112)
    * init: project
    
    * feat: isEmpty_returnsTrue
    
    * feat: enqueue_then_isEmpty_returnsFalse
    
    * feat: enqueue_then_isEmpty_returnsFalse
    
    * feat: enqueue_then_peek_returnsValue
    
    * feat: enqueue_multipleValues_then_returnsFirstValue
    
    * feat: dequeue_throwsException_whenEmpty
    
    * feat: enqueue_then_dequeue_returnsFirstValue
    
    * feat: enqueue_multipleValues_then_dequeue_returnsFirstValue
    
    * refactor: extract to validate empty queue
    
    * test: enqueue_multipleValues_then_dequeue_returnsValuesInOrder
    
    * feat: dequeue_then_peek_moves_to_next
    
    * refactor: in & out

---
+++ poc-challenges/250823/src/test/java/com/tdd/domain/FIFOQueue.java
@@ -0,0 +1,48 @@
+package com.tdd.domain;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.NoSuchElementException;
+
+public class FIFOQueue {
+	private final Deque<Integer> in = new ArrayDeque<>();
+	private final Deque<Integer> out = new ArrayDeque<>();
+
+	public boolean isEmpty() {
+		return in.isEmpty() && out.isEmpty();
+	}
+
+	public void enqueue(int i) {
+		in.push(i);
+	}
+
+	public int peek() {
+		validateEmptyQueue();
+
+		moveInOrder();
+
+		return out.peek();
+	}
+
+	public int dequeue() {
+		validateEmptyQueue();
+
+		moveInOrder();
+
+		return out.pop();
+	}
+
+	private void moveInOrder() {
+		if (out.isEmpty()) {
+			while (!in.isEmpty()) {
+				out.push(in.pop());
+			}
+		}
+	}
+
+	private void validateEmptyQueue() {
+		if (isEmpty()) {
+			throw new NoSuchElementException("queue is empty");
+		}
+	}
+}
