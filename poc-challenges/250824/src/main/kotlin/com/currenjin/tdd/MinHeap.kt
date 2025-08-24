6a5b69033122c1a6ccbfed6a957625b744e016cd
currenjin <hyun0524e@naver.com>
2025-08-24 18:17:56 +0900

    feat: add_multipleValues_poll_returnsValuesInOrder

--- poc-challenges/250824/src/main/kotlin/com/currenjin/tdd/MinHeap.kt
+++ poc-challenges/250824/src/main/kotlin/com/currenjin/tdd/MinHeap.kt
@@ -25,7 +25,34 @@ class MinHeap {
             throw NoSuchElementException("Heap is empty")
         }
 
-        return data.removeFirst()
+        if (data.size == 1) {
+            return data.removeAt(0)
+        }
+
+        val root = data[0]
+        val last = data.removeAt(data.lastIndex)
+        data[0] = last
+        shiftDown(0)
+
+        return root
+    }
+
+    private fun shiftDown(i: Int) {
+        var index = i
+        val size = data.size
+
+        while (true) {
+            val left = index * 2 + 1
+            val right = index + 1
+            var smallest = index
+
+            if (left < size && data[left] < data[smallest]) smallest = left
+            if (right < size && data[right] < data[smallest]) smallest = right
+
+            if (smallest == index) break
+            data[index] = data[smallest].also { data[smallest] = data[index] }
+            index = smallest
+        }
     }
 
     private fun siftUp(lastIndex: Int) {
