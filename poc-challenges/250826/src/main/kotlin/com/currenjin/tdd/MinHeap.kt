2121ad3518883a7c812d81648671d78529d087b5
Hyunjin-Jeong <hyun0524e@naver.com>
2025-08-26 08:41:59 +0900

    Challenge/250826 (#115)
    * init: project
    
    * feat: isEmpty_returnsTrue
    
    * feat: add_then_isEmpty_returnsFalse
    
    * feat: peek_throwsException_whenEmpty
    
    * feat: add_then_peek_returnsValue
    
    * feat: add_multipleValues_then_peek_returnsMinValue
    
    * feat: poll_throwsException_whenEmpty
    
    * feat: add_then_poll_returnsValue
    
    * refactor: extract to validate empty heap
    
    * feat: add_multipleValues_then_poll_returnsMinValueInOrder

---
+++ poc-challenges/250826/src/main/kotlin/com/currenjin/tdd/MinHeap.kt
@@ -0,0 +1,67 @@
+package com.currenjin.tdd
+
+class MinHeap {
+    private val data = arrayListOf<Int>()
+
+    fun isEmpty() = data.isEmpty()
+
+    fun add(i: Int) {
+        data.add(i)
+        siftUp(data.lastIndex)
+    }
+
+    fun peek(): Int {
+        validateEmptyHeap()
+
+        return data[0]
+    }
+
+    fun poll(): Int {
+        validateEmptyHeap()
+
+        if (data.size == 1) return data.removeAt(0)
+
+        val root = data[0]
+        val last = data.removeAt(data.lastIndex)
+        data[0] = last
+        shiftDown(0)
+
+        return root
+    }
+
+    private fun shiftDown(firstIndex: Int) {
+        var index = firstIndex
+        val size = data.size
+
+        while (true) {
+            val left = index * 2 + 1
+            val right = index + 1
+            var smallestIndex = index
+
+            if (left < size && data[left] < data[smallestIndex]) smallestIndex = left
+            if (right < size && data[right] < data[smallestIndex]) smallestIndex = right
+
+            if (smallestIndex == index) break
+            data[index] = data[smallestIndex].also { data[smallestIndex] = data[index] }
+            index = smallestIndex
+        }
+    }
+
+    private fun siftUp(lastIndex: Int) {
+        var index = lastIndex
+
+        while (index > 0) {
+            val parentIndex = (index - 1) / 2
+            if (data[index] < data[parentIndex]) {
+                data[index] = data[parentIndex].also { data[parentIndex] = data[index] }
+                index = parentIndex
+            } else {
+                break
+            }
+        }
+    }
+
+    private fun validateEmptyHeap() {
+        if (isEmpty()) throw NoSuchElementException("Heap is empty")
+    }
+}
