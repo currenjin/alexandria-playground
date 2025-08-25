6e82abd8eb4dc16fa902bc560988d76b47e03afd
currenjin <hyun0524e@naver.com>
2025-08-26 08:32:42 +0900

    feat: add_multipleValues_then_peek_returnsMinValue

--- poc-challenges/250826/src/main/kotlin/com/currenjin/tdd/MinHeap.kt
+++ poc-challenges/250826/src/main/kotlin/com/currenjin/tdd/MinHeap.kt
@@ -7,11 +7,26 @@ class MinHeap {
 
     fun add(i: Int) {
         data.add(i)
+        siftUp(data.lastIndex)
     }
 
     fun peek(): Int {
         if (isEmpty()) throw NoSuchElementException("Heap is empty")
 
         return data[0]
+    }
+
+    private fun siftUp(lastIndex: Int) {
+        var index = lastIndex
+
+        while (index > 0) {
+            val parentIndex = (index - 1) / 2
+            if (data[index] < data[parentIndex]) {
+                data[index] = data[parentIndex].also { data[parentIndex] = data[index] }
+                index = parentIndex
+            } else {
+                break
+            }
+        }
     }
 }
