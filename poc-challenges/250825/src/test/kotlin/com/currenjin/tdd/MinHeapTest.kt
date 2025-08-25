0a771297ed325122e2b30d8cca5e417bc60c3916
currenjin <hyun0524e@naver.com>
2025-08-25 19:12:57 +0900

    feat: add_then_isEmpty_returnsFalse

--- poc-challenges/250825/src/test/kotlin/com/currenjin/tdd/MinHeapTest.kt
+++ poc-challenges/250825/src/test/kotlin/com/currenjin/tdd/MinHeapTest.kt
@@ -1,6 +1,7 @@
 package com.currenjin.tdd
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class MinHeapTest {
@@ -9,5 +10,14 @@ class MinHeapTest {
         val heap = MinHeap()
 
         assertTrue(heap.isEmpty())
+    }
+
+    @Test
+    fun add_then_isEmpty_returnsFalse() {
+        val heap = MinHeap()
+
+        heap.add(1)
+
+        assertFalse(heap.isEmpty())
     }
 }
