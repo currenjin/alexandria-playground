e2ad4c1bc95cf00a33e91b863a99b4780ced4a5c
currenjin <hyun0524e@naver.com>
2025-08-29 15:24:46 +0900

    init: project

---
+++ poc-challenges/250829/src/test/kotlin/com/currenjin/tdd/MoneyTest.kt
@@ -0,0 +1,67 @@
+package com.currenjin.tdd
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class MoneyTest {
+    @Test
+    fun add_two_amounts() {
+        val three = Money.of(3000)
+        val five = Money.of(5000)
+
+        val actual = five + three
+
+        assertEquals(Money.of(8000), actual)
+    }
+
+    @Test
+    fun subtract_two_amounts() {
+        val three = Money.of(3000)
+        val five = Money.of(5000)
+
+        val actual = five - three
+
+        assertEquals(Money.of(2000), actual)
+    }
+
+    @Test
+    fun multiply_money_by_integer() {
+        val five = Money.of(5000)
+
+        val actual = five * 3
+
+        assertEquals(Money.of(15000), actual)
+    }
+
+    @Test
+    fun divide_money_by_integer() {
+        val five = Money.of(5000)
+
+        val actual = five / 5
+
+        assertEquals(Money.of(1000), actual)
+    }
+
+    @Test
+    fun allocate_1000_into_3_parts_distributes_remainder_to_front() {
+        val parts = Money.of(1000).allocate(3)
+
+        assertEquals(
+            listOf(
+                Money.of(334),
+                Money.of(333),
+                Money.of(333),
+            ),
+            parts,
+        )
+    }
+
+    @Test
+    fun money_cannot_be_negative() {
+        assertThrows<IllegalArgumentException> {
+            Money.of(-1)
+        }
+    }
+}
