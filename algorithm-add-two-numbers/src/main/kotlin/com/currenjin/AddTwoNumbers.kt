cf121f9903275db3e0d449bd050e48f33aa5b54d
currenjin <hyun0524e@naver.com>
2025-09-02 12:46:57 +0900

    feat: addOneNumbers_returnsTwoNumbers (+test)

--- algorithm-add-two-numbers/src/main/kotlin/com/currenjin/AddTwoNumbers.kt
+++ algorithm-add-two-numbers/src/main/kotlin/com/currenjin/AddTwoNumbers.kt
@@ -10,7 +10,24 @@ class AddTwoNumbers {
         ): LinkedList<Int> {
             val list = LinkedList<Int>()
             list.add(node1.element() + node2.element())
-            return list
+
+            val numberOfNode1 = node1.reversed().joinToString { it.toString() }.toInt()
+            val numberOfNode2 = node2.reversed().joinToString { it.toString() }.toInt()
+
+            val result = numberOfNode1 + numberOfNode2
+            val splitResult =
+                result
+                    .toString()
+                    .split("")
+                    .mapNotNull {
+                        try {
+                            it.toInt()
+                        } catch (e: NumberFormatException) {
+                            return@mapNotNull null
+                        }
+                    }.reversed()
+
+            return LinkedList(splitResult)
         }
     }
 }
--- algorithm-add-two-numbers/src/test/kotlin/com/currenjin/AddTwoNumbersTest.kt
+++ algorithm-add-two-numbers/src/test/kotlin/com/currenjin/AddTwoNumbersTest.kt
@@ -16,4 +16,16 @@ class AddTwoNumbersTest {
 
         assertEquals(listOf(8), actual)
     }
+
+    @Test
+    fun addOneNumbers_returnsTwoNumbers() {
+        val node1 = LinkedList<Int>()
+        val node2 = LinkedList<Int>()
+        node1.add(4)
+        node2.add(8)
+
+        val actual = AddTwoNumbers.add(node1, node2)
+
+        assertEquals(listOf(2, 1), actual)
+    }
 }
