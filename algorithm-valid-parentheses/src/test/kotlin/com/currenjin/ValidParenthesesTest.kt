bdb257be4aa540bdf1e0cae6b5903cca9cb5859e
Hyunjin-Jeong <hyun0524e@naver.com>
2025-09-03 23:38:41 +0900

    Algorithm/valid parentheses (#126)
    * init: project
    
    * feat: empty_returnsTrue
    
    * feat: single_open_returnsFalse
    
    * refactor: return true early when blank
    
    * feat: single_pair_returnsTrue
    
    * refactor: parentheses condition for {}
    
    * feat: nested_returns_true
    
    * feat: interleaved

---
+++ algorithm-valid-parentheses/src/test/kotlin/com/currenjin/ValidParenthesesTest.kt
@@ -0,0 +1,53 @@
+package com.currenjin
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class ValidParenthesesTest {
+    @Test
+    fun empty_returnsTrue() {
+        val actual = ValidParentheses.isValid("")
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun single_open_returnsFalse() {
+        val actual = ValidParentheses.isValid("{")
+
+        assertFalse(actual)
+    }
+
+    @Test
+    fun single_pair_returnsTrue() {
+        val actual = ValidParentheses.isValid("{}")
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun nested_returns_true() {
+        val actual = ValidParentheses.isValid("{{}}")
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun interleaved_returns_true() {
+        val actual = ValidParentheses.isValid("{{<({<<>>})>}}")
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun interleaved_returns_false() {
+        val actual1 = ValidParentheses.isValid("{{<({<<<}>>})>}}")
+        val actual2 = ValidParentheses.isValid("{{<({<<}>>})>}}")
+        val actual3 = ValidParentheses.isValid("{{<({<<{>>})>}}")
+
+        assertFalse(actual1)
+        assertFalse(actual2)
+        assertFalse(actual3)
+    }
+}
