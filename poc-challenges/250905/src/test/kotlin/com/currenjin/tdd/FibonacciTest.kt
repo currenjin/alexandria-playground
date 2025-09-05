c8335d322219b8fd1c5d8505c5c0b3aa00f586e0
currenjin <hyun0524e@naver.com>
2025-09-05 21:05:16 +0900

    2 returns 1

--- poc-challenges/250905/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
+++ poc-challenges/250905/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
@@ -8,5 +8,6 @@ class FibonacciTest {
     fun fibonacci_test() {
         assertEquals(0, Fibonacci.fib(0))
         assertEquals(1, Fibonacci.fib(1))
+        assertEquals(1, Fibonacci.fib(2))
     }
 }
