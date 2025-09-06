0f932609c2aecd9ec32d73d77576295b665fde0f
currenjin <hyun0524e@naver.com>
2025-09-06 10:46:53 +0900

    refactor: to min condition

--- poc-challenges/250906/src/main/kotlin/com/currenjin/tdd/Factorial.kt
+++ poc-challenges/250906/src/main/kotlin/com/currenjin/tdd/Factorial.kt
@@ -3,7 +3,7 @@ package com.currenjin.tdd
 class Factorial() {
     companion object {
         fun fac(n: Int): Int {
-            if (n <= 2) return n
+            if (n == 1) return 1
             return fac(n - 1) * n
         }
     }
