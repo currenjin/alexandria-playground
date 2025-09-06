6fe374b2354b3ab70d017f38ec7880c9fdc09909
currenjin <hyun0524e@naver.com>
2025-09-06 10:46:01 +0900

    feat: 5 returns 120

--- poc-challenges/250906/src/main/kotlin/com/currenjin/tdd/Factorial.kt
+++ poc-challenges/250906/src/main/kotlin/com/currenjin/tdd/Factorial.kt
@@ -4,7 +4,7 @@ class Factorial() {
     companion object {
         fun fac(n: Int): Int {
             if (n <= 2) return n
-            return (n - 2) * (n - 1) * n
+            return fac(n - 1) * n
         }
     }
 
