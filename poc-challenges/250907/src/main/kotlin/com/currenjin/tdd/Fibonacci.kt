13c3bca06b5eb1d6c257a0281846093e0effb70a
currenjin <hyun0524e@naver.com>
2025-09-07 21:07:51 +0900

    feat: 0 returns 0

---
+++ poc-challenges/250907/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
@@ -0,0 +1,10 @@
+package com.currenjin.tdd
+
+class Fibonacci() {
+    companion object {
+        fun fibonacci(n: Int): Int {
+            return n
+        }
+    }
+
+}
---
+++ poc-challenges/250907/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
@@ -0,0 +1,11 @@
+package com.currenjin.tdd
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FibonacciTest {
+    @Test
+    fun fibonacci() {
+        assertEquals(0, Fibonacci.fibonacci(0))
+    }
+}
