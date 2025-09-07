12655ea5aa7fb3126948f606def87a8c5c6b491e
currenjin <hyun0524e@naver.com>
2025-09-08 08:08:35 +0900

    feat: new_map_is_empty

--- implementation-hash-map/src/main/kotlin/com/currenjin/tdd/MyHashMap.kt
+++ implementation-hash-map/src/main/kotlin/com/currenjin/tdd/MyHashMap.kt
@@ -1,5 +1,9 @@
 package com.currenjin.tdd
 
 class MyHashMap<K: Any, V> {
+    fun isEmpty(): Boolean {
+        return true
+    }
+
     val size: Int = 0
 }
--- implementation-hash-map/src/test/kotlin/com/currenjin/tdd/HashMapTest.kt
+++ implementation-hash-map/src/test/kotlin/com/currenjin/tdd/HashMapTest.kt
@@ -1,6 +1,7 @@
 package com.currenjin.tdd
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class HashMapTest {
@@ -9,5 +10,12 @@ class HashMapTest {
         val map = MyHashMap<String, Int?>()
 
         assertEquals(0, map.size)
+    }
+
+    @Test
+    fun new_map_is_empty() {
+        val map = MyHashMap<String, Int?>()
+
+        assertTrue(map.isEmpty())
     }
 }
