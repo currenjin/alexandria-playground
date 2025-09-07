474933f9082180acc539e2dcddb59465bf21e062
currenjin <hyun0524e@naver.com>
2025-09-07 21:14:21 +0900

    refactor: condition

--- poc-challenges/250907/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
+++ poc-challenges/250907/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
@@ -3,8 +3,7 @@ package com.currenjin.tdd
 class Fibonacci() {
     companion object {
         fun fibonacci(n: Int): Int {
-            if (n == 0) return 0
-            if (n <= 2) return 1
+            if (n <= 1) return n
             return fibonacci(n - 2) + fibonacci(n - 1)
         }
     }
