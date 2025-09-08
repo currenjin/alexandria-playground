86acf0f74d967b5cf3d37606c6b9fb11d857b8db
currenjin <hyun0524e@naver.com>
2025-09-09 08:35:50 +0900

    reset

--- implementation-postfix-notation/src/main/kotlin/com/currenjin/tdd/Postfix.kt
+++ implementation-postfix-notation/src/main/kotlin/com/currenjin/tdd/Postfix.kt
@@ -5,6 +5,10 @@ import java.util.Stack
 class Postfix() {
     companion object {
         fun calculate(stack: Stack<String>): Int {
+            for (operand in stack) {
+
+            }
+
             throw IllegalArgumentException("Stack must have operands to perform calculation")
         }
     }
--- implementation-postfix-notation/src/test/kotlin/com/currenjin/tdd/PostfixNotationTest.kt
+++ implementation-postfix-notation/src/test/kotlin/com/currenjin/tdd/PostfixNotationTest.kt
@@ -1,17 +1,5 @@
 package com.currenjin.tdd
 
-import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.Test
-import java.util.Stack
-
 class PostfixNotationTest {
-    @Test
-    fun throwsException_whenStackDoesNotHaveOperands() {
-        val stack = Stack<String>()
 
-        stack.push("10")
-        stack.push("20")
-
-        assertThrows(IllegalArgumentException::class.java) { Postfix.calculate(stack) }
-    }
 }
