35255dc0b5fbc9e35ca77276f2491272007c9c97
currenjin <hyun0524e@naver.com>
2025-09-09 12:06:56 +0900

    feat: '6 4 +' returns 10

--- implementation-postfix-notation/src/main/kotlin/com/currenjin/tdd/Postfix.kt
+++ implementation-postfix-notation/src/main/kotlin/com/currenjin/tdd/Postfix.kt
@@ -1,5 +1,10 @@
 package com.currenjin.tdd
 
 class Postfix() {
+    companion object {
+        fun calculate(postfix: String): Int {
+            return 10
+        }
+    }
 
 }
--- implementation-postfix-notation/src/test/kotlin/com/currenjin/tdd/PostfixNotationTest.kt
+++ implementation-postfix-notation/src/test/kotlin/com/currenjin/tdd/PostfixNotationTest.kt
@@ -1,5 +1,15 @@
 package com.currenjin.tdd
 
-class PostfixNotationTest {
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
+class PostfixNotationTest {
+    @ParameterizedTest
+    @CsvSource("10, 6 4 +")
+    fun postfixTest(expected: Int, postfix: String) {
+        val actual = Postfix.calculate(postfix)
+
+        assertEquals(expected, actual)
+    }
 }
