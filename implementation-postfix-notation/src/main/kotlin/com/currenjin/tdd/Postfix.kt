2691ccf925399c33c6ca591012c70f97b771b760
currenjin <hyun0524e@naver.com>
2025-09-09 12:00:55 +0900

    reset

--- implementation-postfix-notation/src/main/kotlin/com/currenjin/tdd/Postfix.kt
+++ implementation-postfix-notation/src/main/kotlin/com/currenjin/tdd/Postfix.kt
@@ -1,16 +1,5 @@
 package com.currenjin.tdd
 
-import java.util.Stack
-
 class Postfix() {
-    companion object {
-        fun calculate(stack: Stack<String>): Int {
-            for (operand in stack) {
-
-            }
-
-            throw IllegalArgumentException("Stack must have operands to perform calculation")
-        }
-    }
 
 }
