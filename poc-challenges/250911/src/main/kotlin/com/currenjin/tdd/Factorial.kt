b0d3641711189056b1dedb81af4fe63d22bd28f1
currenjin <hyun0524e@naver.com>
2025-09-11 08:28:27 +0900

    feat: 1 returns 1

--- poc-challenges/250911/src/main/kotlin/com/currenjin/tdd/Factorial.kt
+++ poc-challenges/250911/src/main/kotlin/com/currenjin/tdd/Factorial.kt
@@ -1,5 +1,10 @@
 package com.currenjin.tdd
 
 class Factorial() {
+    companion object {
+        fun fac(n: Int): Int {
+            return n
+        }
+    }
 
 }
--- poc-challenges/250911/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
+++ poc-challenges/250911/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
@@ -1,4 +1,11 @@
 package com.currenjin.tdd
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class FactorialTest {
+    @Test
+    fun factorial() {
+        assertEquals(1, Factorial.fac(1))
+    }
 }
