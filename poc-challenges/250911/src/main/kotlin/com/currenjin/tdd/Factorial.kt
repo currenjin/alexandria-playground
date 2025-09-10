b7a7939d96c1c41cbb32dd3b6454b9e5f3f0413e
Hyunjin-Jeong <hyun0524e@naver.com>
2025-09-11 08:33:36 +0900

    Challenge/250911 (#136)
    * init: project
    
    * feat: 1 returns 1
    
    * feat: 2 returns 2
    
    * refactor: to parameterized test with csv source
    
    * feat: 3 returns 6
    
    * refactor: change returning to 3 * 2
    
    * refactor: change returning to 3 * fac(n - 1)
    
    * feat: 4 returns 24
    
    * test: add other cases

---
+++ poc-challenges/250911/src/main/kotlin/com/currenjin/tdd/Factorial.kt
@@ -0,0 +1,11 @@
+package com.currenjin.tdd
+
+class Factorial() {
+    companion object {
+        fun fac(n: Int): Int {
+            if (n <= 2) return n
+            return n * fac(n - 1)
+        }
+    }
+
+}
