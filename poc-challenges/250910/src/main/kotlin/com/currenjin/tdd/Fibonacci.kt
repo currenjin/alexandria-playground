7f750dbbd2e2d70236ecea414cd5a7f067a4e488
currenjin <hyun0524e@naver.com>
2025-09-10 12:35:59 +0900

    refactor: remove condition for number 2

--- poc-challenges/250910/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
+++ poc-challenges/250910/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
@@ -4,7 +4,6 @@ class Fibonacci() {
     companion object {
         fun fib(n: Int): Int {
             if (n <= 1) return n
-            if (n == 2) return 1
             return fib(n - 2) + fib(n - 1)
         }
     }
