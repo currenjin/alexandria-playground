17fa32681ad2a97b90a2259e00cc9f11c5a0a3e8
currenjin <hyun0524e@naver.com>
2025-09-11 08:33:06 +0900

    test: add other cases

--- poc-challenges/250911/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
+++ poc-challenges/250911/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class FactorialTest {
     @ParameterizedTest
-    @CsvSource("1, 1", "2, 2", "3, 6", "4, 24")
+    @CsvSource("1, 1", "2, 2", "3, 6", "4, 24", "5, 120", "6, 720")
     fun factorial(input: Int, expected: Int) {
         assertEquals(expected, Factorial.fac(input))
     }
