7c517cd7818e9e6a8f7466dc18a5fa0bf6a5bec8
currenjin <hyun0524e@naver.com>
2025-09-11 08:29:40 +0900

    refactor: to parameterized test with csv source

--- poc-challenges/250911/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
+++ poc-challenges/250911/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
@@ -1,12 +1,13 @@
 package com.currenjin.tdd
 
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class FactorialTest {
-    @Test
-    fun factorial() {
-        assertEquals(1, Factorial.fac(1))
-        assertEquals(2, Factorial.fac(2))
+    @ParameterizedTest
+    @CsvSource("1, 1", "2, 2")
+    fun factorial(input: Int, expected: Int) {
+        assertEquals(expected, Factorial.fac(input))
     }
 }
