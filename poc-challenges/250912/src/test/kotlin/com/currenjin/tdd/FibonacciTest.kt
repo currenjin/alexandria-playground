06bcc90193b61b66b32b45f73104648febdeff79
Hyunjin-Jeong <hyun0524e@naver.com>
2025-09-12 12:58:13 +0900

    Challenge/250912 (#137)
    * init: project
    
    * feat: 0, fib(0)
    
    * feat: 1, fib(1)
    
    * refactor: to parameterized test
    
    * feat: 1, fib(2)
    
    * refactor: to fib(n-1)
    
    * feat: 2, fib(3)
    
    * test: add cases

---
+++ poc-challenges/250912/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
@@ -0,0 +1,13 @@
+package com.currenjin.tdd
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class FibonacciTest {
+    @ParameterizedTest
+    @CsvSource("0,0", "1,1", "2, 1", "3, 2", "4, 3", "5, 5", "6, 8", "7, 13")
+    fun fib(number: Int, expected: Int) {
+        assertEquals(expected, Fibonacci.fib(number))
+    }
+}
