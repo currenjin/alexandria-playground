cb926607ff939c924a219bb748d30255670d820f
currenjin <hyun0524e@naver.com>
2025-09-12 12:53:38 +0900

    feat: 0, fib(0)

--- poc-challenges/250912/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
+++ poc-challenges/250912/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
@@ -1,4 +1,11 @@
 package com.currenjin.tdd
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
 class FibonacciTest {
+    @Test
+    fun fib() {
+        assertEquals(0, Fibonacci.fib(0))
+    }
 }
