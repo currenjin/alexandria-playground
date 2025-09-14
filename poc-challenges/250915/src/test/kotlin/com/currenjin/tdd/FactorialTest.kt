65b816bdf5fa0c167d43e349efea801921d310b1
currenjin <hyun0524e@naver.com>
2025-09-15 08:20:52 +0900

    test: add cases

--- poc-challenges/250915/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
+++ poc-challenges/250915/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
@@ -6,7 +6,7 @@ import kotlin.test.assertEquals
 
 class FactorialTest {
     @ParameterizedTest
-    @CsvSource("1,1", "2,2", "3,6", "4,24")
+    @CsvSource("1,1", "2,2", "3,6", "4,24", "5,120", "6,720", "7,5040")
     fun fac(input: Int, expected: Int) {
         assertEquals(expected, Factorial.cal(input))
     }
