caa4a1fae857d72bdace8f43ed3cf4a5360a5a96
currenjin <hyun0524e@naver.com>
2025-09-14 22:39:46 +0900

    feat: 5, result 5

--- poc-challenges/250914/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
+++ poc-challenges/250914/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
@@ -5,7 +5,7 @@ class Fibonacci {
         fun fib(n: Int): Int {
             if (n == 0) return 0
             if (n <= 2) return 1
-            return 1 + fib(n - 1)
+            return fib(n - 2) + fib(n - 1)
         }
     }
 
