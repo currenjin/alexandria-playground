206e69aa87b9b290805fddde77dd8373de278157
Hyunjin-Jeong <hyun0524e@naver.com>
2025-09-17 23:10:15 +0900

    Challenge/250917 (#141)
    * init: project
    
    * feat: 1, result 1
    
    * feat: 2, result 2
    
    * feat: 3, result 6
    
    * refactor: to 2*3
    
    * feat: 4, result 24
    
    * feat: 5, result 120

---
+++ poc-challenges/250917/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
@@ -0,0 +1,13 @@
+package com.currenjin.tdd
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class FactorialTest {
+    @ParameterizedTest
+    @CsvSource("1, 1", "2, 2", "6, 3", "24, 4", "120, 5")
+    fun factorial(expected: Int, input: Int) {
+        assertEquals(expected, Factorial.fac(input))
+    }
+}
