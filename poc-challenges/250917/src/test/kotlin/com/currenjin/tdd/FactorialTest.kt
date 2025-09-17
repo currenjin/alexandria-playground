c801da7f5a0ec4c31cceac9dd9d90e3e1273daa3
currenjin <hyun0524e@naver.com>
2025-09-17 23:09:36 +0900

    feat: 4, result 24

--- poc-challenges/250917/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
+++ poc-challenges/250917/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class FactorialTest {
     @ParameterizedTest
-    @CsvSource("1, 1", "2, 2", "6, 3")
+    @CsvSource("1, 1", "2, 2", "6, 3", "24, 4")
     fun factorial(expected: Int, input: Int) {
         assertEquals(expected, Factorial.fac(input))
     }
