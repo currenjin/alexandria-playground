13ce8828b62cca3092ee703d054fd494ad5bd255
currenjin <hyun0524e@naver.com>
2025-09-18 12:58:02 +0900

    feat: 4 returns 3

--- poc-challenges/250918/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
+++ poc-challenges/250918/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class FibonacciTest {
     @ParameterizedTest
-    @CsvSource("0,0", "1,1", "2,1", "3,2")
+    @CsvSource("0,0", "1,1", "2,1", "3,2", "4,3")
     fun fibonacci(input: Int, expected: Int) {
         assertEquals(expected, Fibonacci.cal(input))
     }
