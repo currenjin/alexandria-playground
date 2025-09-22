ad9ddaef4cb94c455a30ba8fd28e65139e340ca9
currenjin <hyun0524e@naver.com>
2025-09-23 08:24:22 +0900

    refactor: Euclidean class to object

--- implementation-euclidean-gcd/src/main/kotlin/com/currenjin/Euclidean.kt
+++ implementation-euclidean-gcd/src/main/kotlin/com/currenjin/Euclidean.kt
@@ -2,21 +2,19 @@ package com.currenjin
 
 import kotlin.math.abs
 
-class Euclidean {
-    companion object {
-        fun gcd(
-            n1: Int,
-            n2: Int,
-        ): Int = gcd(n1.toLong(), n2.toLong()).toInt()
+object Euclidean {
+    fun gcd(
+        n1: Int,
+        n2: Int,
+    ): Int = gcd(n1.toLong(), n2.toLong()).toInt()
 
-        fun gcd(
-            n1: Long,
-            n2: Long,
-        ): Long {
-            val x = abs(n1)
-            val y = abs(n2)
-            if (y == 0L) return x
-            return gcd(y, x % y)
-        }
+    fun gcd(
+        n1: Long,
+        n2: Long,
+    ): Long {
+        val x = abs(n1)
+        val y = abs(n2)
+        if (y == 0L) return x
+        return gcd(y, x % y)
     }
 }
