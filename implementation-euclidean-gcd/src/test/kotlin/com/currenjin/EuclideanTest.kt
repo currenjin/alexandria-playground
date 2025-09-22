c2b41ab9151f7bbe25388fabf783719d0202eaf1
currenjin <hyun0524e@naver.com>
2025-09-23 08:15:18 +0900

    feat: gcd_of_9_and_6_is_3

--- implementation-euclidean-gcd/src/test/kotlin/com/currenjin/EuclideanTest.kt
+++ implementation-euclidean-gcd/src/test/kotlin/com/currenjin/EuclideanTest.kt
@@ -28,4 +28,9 @@ class EuclideanTest {
     fun gcd_of_4_and_2_is_2() {
         assertEquals(2, Euclidean.gcd(4, 2))
     }
+
+    @Test
+    fun gcd_of_9_and_6_is_3() {
+        assertEquals(3, Euclidean.gcd(9, 6))
+    }
 }
