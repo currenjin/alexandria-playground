964d94824189dd2b83fe3c39fa31ac6dc46279e3
currenjin <hyun0524e@naver.com>
2025-09-23 08:11:22 +0900

    feat: gcd_of_2_and_1_is_1

--- implementation-euclidean-gcd/src/test/kotlin/com/currenjin/EuclideanTest.kt
+++ implementation-euclidean-gcd/src/test/kotlin/com/currenjin/EuclideanTest.kt
@@ -8,4 +8,9 @@ class EuclideanTest {
     fun gcd_of_1_and_1_is_1() {
         assertEquals(1, Euclidean.gcd(1, 1))
     }
+
+    @Test
+    fun gcd_of_2_and_1_is_1() {
+        assertEquals(1, Euclidean.gcd(2, 1))
+    }
 }
