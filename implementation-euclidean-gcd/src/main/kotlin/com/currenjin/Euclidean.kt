4902e993b6754a59324f161b46cd5ee96210734f
currenjin <hyun0524e@naver.com>
2025-09-23 08:25:56 +0900

    comment: Euclidean method

--- implementation-euclidean-gcd/src/main/kotlin/com/currenjin/Euclidean.kt
+++ implementation-euclidean-gcd/src/main/kotlin/com/currenjin/Euclidean.kt
@@ -3,11 +3,25 @@ package com.currenjin
 import kotlin.math.abs
 
 object Euclidean {
+    /**
+     * Calculates the greatest common divisor (GCD) of two numbers using the Euclidean algorithm.
+     *
+     * @param n1 first integer
+     * @param n2 second integer
+     * @return greatest common divisor of n1 and n2
+     */
     fun gcd(
         n1: Int,
         n2: Int,
     ): Int = gcd(n1.toLong(), n2.toLong()).toInt()
 
+    /**
+     * Calculates the greatest common divisor (GCD) of two numbers using the Euclidean algorithm.
+     *
+     * @param n1 first long
+     * @param n2 second long
+     * @return greatest common divisor of n1 and n2
+     */
     fun gcd(
         n1: Long,
         n2: Long,
