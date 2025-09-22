5ff31d0873ed6054ea696d132806203ff27b0aa0
currenjin <hyun0524e@naver.com>
2025-09-23 08:14:40 +0900

    feat: gcd_of_4_and_2_is_2

--- implementation-euclidean-gcd/src/main/kotlin/com/currenjin/Euclidean.kt
+++ implementation-euclidean-gcd/src/main/kotlin/com/currenjin/Euclidean.kt
@@ -6,8 +6,8 @@ class Euclidean {
             n1: Int,
             n2: Int,
         ): Int {
-            if (n1 == 2 && n2 == 2) return 2
-            return 1
+            if (n2 == 0) return n1
+            return gcd(n2, n1 % n2)
         }
     }
 }
--- implementation-euclidean-gcd/src/test/kotlin/com/currenjin/EuclideanTest.kt
+++ implementation-euclidean-gcd/src/test/kotlin/com/currenjin/EuclideanTest.kt
@@ -23,4 +23,9 @@ class EuclideanTest {
     fun gcd_of_3_and_2_is_1() {
         assertEquals(1, Euclidean.gcd(3, 2))
     }
+
+    @Test
+    fun gcd_of_4_and_2_is_2() {
+        assertEquals(2, Euclidean.gcd(4, 2))
+    }
 }
