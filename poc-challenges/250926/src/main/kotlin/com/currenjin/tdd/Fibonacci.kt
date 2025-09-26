09ac679d3b51b09e9d68a4ccbdb643f513020ff6
currenjin <hyun0524e@naver.com>
2025-09-26 23:08:31 +0900

    remove condition

--- poc-challenges/250926/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
+++ poc-challenges/250926/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
@@ -4,7 +4,6 @@ class Fibonacci {
     companion object {
         fun fib(number: Int): Int {
             if (number <= 1) return number
-            if (number == 2) return 1
             return fib(number - 2) + fib(number - 1)
         }
     }
