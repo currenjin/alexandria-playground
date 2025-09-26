1a1bba78ec915e4ef1c03d3a5c4d9cb02a28b461
currenjin <hyun0524e@naver.com>
2025-09-26 23:07:53 +0900

    add cases

--- poc-challenges/250926/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
+++ poc-challenges/250926/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource
 
 class FibonacciTest {
     @ParameterizedTest
-    @CsvSource("0,0", "1,1", "2,1", "3,2", "4,3", "5,5")
+    @CsvSource("0,0", "1,1", "2,1", "3,2", "4,3", "5,5", "6,8", "7,13", "8,21")
     fun fibonacci(input: Int, expected: Int) {
         assertEquals(expected, Fibonacci.fib(input))
     }
