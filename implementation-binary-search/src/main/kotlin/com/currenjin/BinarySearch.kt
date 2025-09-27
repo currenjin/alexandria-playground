c718d674c8870be73c97d9c55772f6fac1922ed3
currenjin <hyun0524e@naver.com>
2025-09-27 22:52:45 +0900

    feat: class to object

--- implementation-binary-search/src/main/kotlin/com/currenjin/BinarySearch.kt
+++ implementation-binary-search/src/main/kotlin/com/currenjin/BinarySearch.kt
@@ -2,39 +2,37 @@ package com.currenjin
 
 private const val NOT_FOUND = -1
 
-class BinarySearch {
-    companion object {
-        fun <T : Comparable<T>>search(array: Array<T>, target: T): Int =
-            searchInternal(array, target, moveLeftOnMatch = true)
+object BinarySearch {
+    fun <T : Comparable<T>>search(array: Array<T>, target: T): Int =
+        searchInternal(array, target, moveLeftOnMatch = true)
 
-        fun <T : Comparable<T>>searchLast(array: Array<T>, target: T): Int =
-            searchInternal(array, target, moveLeftOnMatch = false)
+    fun <T : Comparable<T>>searchLast(array: Array<T>, target: T): Int =
+        searchInternal(array, target, moveLeftOnMatch = false)
 
-        private fun <T : Comparable<T>>searchInternal(
-            array: Array<T>,
-            target: T,
-            moveLeftOnMatch: Boolean
-        ): Int {
-            var left = 0
-            var right = array.size - 1
-            var result = NOT_FOUND
+    private fun <T : Comparable<T>>searchInternal(
+        array: Array<T>,
+        target: T,
+        moveLeftOnMatch: Boolean
+    ): Int {
+        var left = 0
+        var right = array.size - 1
+        var result = NOT_FOUND
 
-            while (left <= right) {
-                val mid = left + (right - left) / 2
-                when {
-                    array[mid] == target -> {
-                        result = mid
-                        if (moveLeftOnMatch) {
-                            right = mid - 1
-                        } else {
-                            left = mid + 1
-                        }
+        while (left <= right) {
+            val mid = left + (right - left) / 2
+            when {
+                array[mid] == target -> {
+                    result = mid
+                    if (moveLeftOnMatch) {
+                        right = mid - 1
+                    } else {
+                        left = mid + 1
                     }
-                    target < array[mid] -> right = mid - 1
-                    else -> left = mid + 1
                 }
+                target < array[mid] -> right = mid - 1
+                else -> left = mid + 1
             }
-            return result
         }
+        return result
     }
 }
