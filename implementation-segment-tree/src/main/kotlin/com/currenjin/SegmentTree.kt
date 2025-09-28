4f933f1d0cdb5c7943f862041951cf6c335f1a4e
currenjin <hyun0524e@naver.com>
2025-09-29 08:47:29 +0900

    feat: implement updateTree to reflect changes in segment tree

--- implementation-segment-tree/src/main/kotlin/com/currenjin/SegmentTree.kt
+++ implementation-segment-tree/src/main/kotlin/com/currenjin/SegmentTree.kt
@@ -13,20 +13,47 @@ class SegmentTree(
     private fun buildTree(node: Int, start: Int, end: Int) {
         if (start == end) {
             tree[node] = data[start]
+        } else {
+            val mid = (start + end) / 2
+            buildTree(node * 2, start, mid)
+            buildTree(node * 2 + 1, mid + 1, end)
+            tree[node] = tree[node * 2] + tree[node * 2 + 1]
         }
     }
 
     fun query(left: Int, right: Int): Int {
-        var sum = 0
+        return queryTree(1, 0, n - 1, left, right)
+    }
 
-        for (i in left..right) {
-            sum += data[i]
+    private fun queryTree(node: Int, start: Int, end: Int, left: Int, right: Int): Int {
+        if (right < start || end < left) {
+            return 0
         }
-
-        return sum
+        if (left <= start && end <= right) {
+            return tree[node]
+        }
+        val mid = (start + end) / 2
+        val lSum = queryTree(node * 2, start, mid, left, right)
+        val rSum = queryTree(node * 2 + 1, mid + 1, end, left, right)
+        return lSum + rSum
     }
 
     fun update(index: Int, value: Int) {
-        data[index] = value
+        updateTree(1, 0, n - 1, index, value)
+    }
+
+    private fun updateTree(node: Int, start: Int, end: Int, idx: Int, value: Int) {
+        if (start == end) {
+            data[idx] = value
+            tree[node] = value
+        } else {
+            val mid = (start + end) / 2
+            if (idx <= mid) {
+                updateTree(node * 2, start, mid, idx, value)
+            } else {
+                updateTree(node * 2 + 1, mid + 1, end, idx, value)
+            }
+            tree[node] = tree[node * 2] + tree[node * 2 + 1]
+        }
     }
 }
