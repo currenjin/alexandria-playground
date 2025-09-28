d46952f72ececc68eb5ebc58c72f65f9eabf7efb
currenjin <hyun0524e@naver.com>
2025-09-29 08:44:13 +0900

    feat: initialize tree array and build leaf nodes

--- implementation-segment-tree/src/main/kotlin/com/currenjin/SegmentTree.kt
+++ implementation-segment-tree/src/main/kotlin/com/currenjin/SegmentTree.kt
@@ -3,6 +3,19 @@ package com.currenjin
 class SegmentTree(
     private val data: Array<Int>
 ) {
+    private val n = data.size
+    private val tree = IntArray(4 * n)
+
+    init {
+        buildTree(1, 0, n - 1)
+    }
+
+    private fun buildTree(node: Int, start: Int, end: Int) {
+        if (start == end) {
+            tree[node] = data[start]
+        }
+    }
+
     fun query(left: Int, right: Int): Int {
         var sum = 0
 
