38c3d402128092c8bb9d1f4784d14e1cca8a990e
currenjin <hyun0524e@naver.com>
2025-09-28 23:42:35 +0900

    refactor: extract to swap

--- implementation-insertion-sort/src/main/kotlin/com/currenjin/InsertionSort.kt
+++ implementation-insertion-sort/src/main/kotlin/com/currenjin/InsertionSort.kt
@@ -5,12 +5,16 @@ object InsertionSort {
         for (i in 1 until array.size) {
             var j = i
             while (j > 0 && array[j - 1] > array[j]) {
-                val temp = array[j]
-                array[j] = array[j - 1]
-                array[j - 1] = temp
+                swap(array, j - 1, j)
                 j--
             }
         }
         return array
+    }
+
+    private fun swap(array: Array<Int>, i: Int, j: Int) {
+        val temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
     }
 }
