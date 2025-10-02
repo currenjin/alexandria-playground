7a971e49653528eecc6f5c812d9d9d148b3ac7ce
currenjin <hyun0524e@naver.com>
2025-10-02 20:38:05 +0900

    feat: add_one_then_peek_returns_it

--- implementation-heap-by-array/src/main/kotlin/com/currenjin/MinHeap.kt
+++ implementation-heap-by-array/src/main/kotlin/com/currenjin/MinHeap.kt
@@ -1,11 +1,15 @@
 package com.currenjin
 
 class MinHeap {
+    private var top: Int? = null
     private var count: Int = 0
 
     fun isEmpty(): Boolean = count == 0
 
     fun add(value: Int) {
         count += 1
+        top = value
     }
+
+    fun peek(): Int = top!!
 }
--- implementation-heap-by-array/src/test/kotlin/com/currenjin/MinHeapTest.kt
+++ implementation-heap-by-array/src/test/kotlin/com/currenjin/MinHeapTest.kt
@@ -1,5 +1,6 @@
 package com.currenjin
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -19,5 +20,14 @@ class MinHeapTest {
         heap.add(1)
 
         assertFalse(heap.isEmpty())
+    }
+
+    @Test
+    fun add_one_then_peek_returns_it() {
+        val heap = MinHeap()
+
+        heap.add(1)
+
+        assertEquals(1, heap.peek())
     }
 }
