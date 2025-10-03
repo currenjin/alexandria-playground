667d9c097d5366c58bf3f3f8a556233f86d82864
Hyunjin-Jeong <hyun0524e@naver.com>
2025-10-03 23:08:41 +0900

    Challenge/251003 heap by array (#158)
    * feat: after_poll_peek_returns_remaining_element
    
    * feat: poll_three_elements_returns_in_ascending_order

--- implementation-heap-by-array/src/test/kotlin/com/currenjin/MinHeapTest.kt
+++ implementation-heap-by-array/src/test/kotlin/com/currenjin/MinHeapTest.kt
@@ -51,4 +51,30 @@ class MinHeapTest {
         assertEquals(1, polled)
         assertTrue(heap.isEmpty())
     }
+
+    @Test
+    fun after_poll_peek_returns_remaining_element() {
+        val heap = MinHeap()
+
+        heap.add(2)
+        heap.add(1)
+
+        val first = heap.poll()
+        assertEquals(1, first)
+        assertEquals(2, heap.peek())
+    }
+
+    @Test
+    fun poll_three_elements_returns_in_ascending_order() {
+        val heap = MinHeap()
+
+        heap.add(3)
+        heap.add(1)
+        heap.add(2)
+
+        assertEquals(1, heap.poll())
+        assertEquals(2, heap.poll())
+        assertEquals(3, heap.poll())
+        assertTrue(heap.isEmpty())
+    }
 }
