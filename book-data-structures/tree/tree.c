7a08175bcc15b3e4613be3bf2b23f1df96b83cc3
currenjin <hyun0524e@naver.com>
2025-10-08 17:43:18 +0900

    add: tree

--- book-data-structures/tree/tree.c
+++ book-data-structures/tree/tree.c
@@ -7,9 +7,61 @@ typedef struct node {
     char data;
 } node;
 
-node* create(void) {
-    node *root = malloc(sizeof(node));
-    return root;
+node* create(const char data) {
+    node* newNode = malloc(sizeof(node));
+    newNode -> data = data;
+    newNode -> left = NULL;
+    newNode -> right = NULL;
+    return newNode;
+}
+
+node *insert(node *here, node *it) {
+    if (here == NULL) {
+        here = it;
+        return here;
+    }
+
+    if (here -> left == NULL) {
+        here -> left = it;
+        return here;
+    }
+
+    here -> right = it;
+    return here;
+}
+
+node *searchParent(node *root, node *it) {
+    if (root == NULL || it == NULL) return NULL;
+
+    if (root -> left == it || root -> right == it) return root;
+
+    node* found = searchParent(root -> left, it);
+    if (found != NULL) return found;
+
+    return searchParent(root -> right, it);
+}
+
+node *delete(node *root, node *it, const char direction) {
+    node *parent = searchParent(root, it);
+
+    if (parent == NULL) {
+        printf("Cannot delete\n");
+        return NULL;
+    }
+
+    if (direction == 'L') {
+        parent -> left = NULL;
+        free(parent -> left);
+        return it;
+    }
+
+    if (direction == 'R') {
+        parent -> right = NULL;
+        free(parent -> right);
+        return it;
+    }
+
+    return NULL;
 }
 
 void preOrder(const node* root) {
@@ -20,10 +72,65 @@ void preOrder(const node* root) {
     }
 }
 
+void inOrder(const node *root) {
+    if (root != NULL) {
+        inOrder(root -> left);
+        printf("%c ", root -> data);
+        inOrder(root -> right);
+    }
+}
+
+void postOrder(const node *root) {
+    if (root != NULL) {
+        postOrder(root -> left);
+        postOrder(root -> right);
+        printf("%c ", root -> data);
+    }
+}
+
+int getNodeCount(node *root) {
+    int num = 0;
+    if (root == NULL) return num;
+
+    num = 1;
+    num += getNodeCount(root -> left) + getNodeCount(root -> right);
+    return num;
+}
+
 int main() {
-    node *node = create();
+    node* a = create('A');
+    node* b = create('B');
+    node* c = create('C');
+    node* d = create('D');
+    node* e = create('E');
 
-    preOrder(node);
+    insert(b, d);
+    insert(c, e);
+    insert(a, b);
+    insert(a, c);
 
-    free(node);
+    const int firstNodeCount = getNodeCount(a);
+    printf("nodeCount: %d\n", firstNodeCount);
+
+    printf("preOrder: ");
+    preOrder(a);
+    printf("\n");
+
+    printf("inOrder: ");
+    inOrder(a);
+    printf("\n");
+
+    printf("postOrder: ");
+    postOrder(a);
+    printf("\n");
+
+    delete(a, e, 'L');
+    printf("preOrder: ");
+    preOrder(a);
+    printf("\n");
+
+    const int secoundNodeCount = getNodeCount(a);
+    printf("nodeCount: %d\n", secoundNodeCount);
+
+    free(a);
 }
