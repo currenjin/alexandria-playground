f31f5ef778b8369da379eed95ab6a404647e77c8
currenjin <hyun0524e@naver.com>
2025-10-12 22:04:45 +0900

    refactor: extract connection maker method

--- book-toby-spring/src/main/java/com/currenjin/ch01/dao/DaoFactory.java
+++ book-toby-spring/src/main/java/com/currenjin/ch01/dao/DaoFactory.java
@@ -2,20 +2,22 @@ package com.currenjin.ch01.dao;
 
 public class DaoFactory {
 	public UserDao userDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new DUserDao(connectionMaker);
+		UserDao userDao = new DUserDao(connectionMaker());
 		return userDao;
 	}
 
 	public AccountDao accountDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		AccountDao accountDao = new AccountDao(connectionMaker);
+		AccountDao accountDao = new AccountDao(connectionMaker());
 		return accountDao;
 	}
 
 	public MessageDao messageDao() {
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		MessageDao messageDao = new MessageDao(connectionMaker);
+		MessageDao messageDao = new MessageDao(connectionMaker());
 		return messageDao;
+	}
+
+	private static ConnectionMaker connectionMaker() {
+		ConnectionMaker connectionMaker = new DConnectionMaker();
+		return connectionMaker;
 	}
 }
