6ee4327d698e604fa06bd181ebe417b81b89354f
currenjin <hyun0524e@naver.com>
2025-11-14 12:54:51 +0900

    feat: user-service in phase2

---
+++ book-building-microservices/examples/phase2-migration/user-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+-- Users
+INSERT INTO users (id, email, username, created_at) VALUES (1, 'alice@music.com', 'alice', CURRENT_TIMESTAMP);
+INSERT INTO users (id, email, username, created_at) VALUES (2, 'bob@music.com', 'bob', CURRENT_TIMESTAMP);
+INSERT INTO users (id, email, username, created_at) VALUES (3, 'charlie@music.com', 'charlie', CURRENT_TIMESTAMP);
