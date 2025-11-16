e9138ee269f0d6a97be32acc067cf60a57dc5220
currenjin <hyun0524e@naver.com>
2025-11-16 15:39:56 +0900

    rename: user -> gateway

--- book-building-microservices/examples/phase2-migration/api-gateway/src/main/java/com/currenjin/gateway/GatewayApplication.java
+++ book-building-microservices/examples/phase2-migration/api-gateway/src/main/java/com/currenjin/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.currenjin.user;
+package com.currenjin.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
