d840a556fe6f3ac49b4ce8ba72db1d78a1af0c45
currenjin <hyun0524e@naver.com>
2025-11-16 16:45:32 +0900

    feat: add Bean Annotation to customRoute method

--- book-building-microservices/examples/phase2-migration/api-gateway/src/main/java/com/currenjin/gateway/RouteConfig.java
+++ book-building-microservices/examples/phase2-migration/api-gateway/src/main/java/com/currenjin/gateway/RouteConfig.java
@@ -3,6 +3,7 @@ package com.currenjin.gateway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -16,6 +17,7 @@ public class RouteConfig {
 	@Value("${services.user-service.url}")
 	private String userServiceUrl;
 
+	@Bean
 	public RouteLocator customRoute(RouteLocatorBuilder builder) {
 		return builder.routes()
 			.route("users", r -> r.path("/api/users/**")
