55c3e67ea3f26cbe170037d23c263d17a4c2c05e
Hyunjin-Jeong <hyun0524e@naver.com>
2025-11-18 12:36:27 +0900

    Challenge/251118 building microservices (#202)
    * fix: customRouteLocator
    
    * remove: UserServiceRoutingFilter
    
    * feat: api-gateway
    
    * feat: monolith
    
    * feat: user-service
    
    * chore: docker-compose
    
    * remove: unused import in user-service
    
    * chore: target -> services
    
    * fix: remove unused request logger and add log
    
    * chore: changes user-service-enabled to true

--- book-building-microservices/examples/phase2-migration/api-gateway/src/main/java/com/currenjin/gateway/RouteConfig.java
+++ book-building-microservices/examples/phase2-migration/api-gateway/src/main/java/com/currenjin/gateway/RouteConfig.java
@@ -1,31 +1,38 @@
 package com.currenjin.gateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class RouteConfig {
+
 	@Value("${feature.toggle.user-service-enabled}")
 	private boolean userServiceEnabled;
 
-	@Value("${services.monolith.url}")
+	@Value("${target.monolith.url}")
 	private String monolithUrl;
 
-	@Value("${services.user-service.url}")
+	@Value("${target.user-service.url}")
 	private String userServiceUrl;
 
 	@Bean
-	public RouteLocator customRoute(RouteLocatorBuilder builder) {
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		String userApiTarget = userServiceEnabled ? userServiceUrl : monolithUrl;
+
+		log.info("User API routing to: {} (userServiceEnabled={})",
+				userApiTarget, userServiceEnabled);
+
 		return builder.routes()
-			.route("users", r -> r.path("/api/users/**")
-				.filters(f -> f.filter(new UserServiceRoutingFilter(
-					userServiceEnabled, monolithUrl, userServiceUrl)))
-				.uri("no://op"))
-			.route("others", r -> r.path("/**")
-				.uri(monolithUrl))
-			.build();
+				.route("user-api", route -> route.path("/api/users/**")
+						.filters(f -> f.addRequestHeader("X-Routed-By", "API-Gateway"))
+						.uri(userApiTarget))
+				.route("others", route -> route.path("/**")
+						.uri(monolithUrl))
+				.build();
 	}
 }
