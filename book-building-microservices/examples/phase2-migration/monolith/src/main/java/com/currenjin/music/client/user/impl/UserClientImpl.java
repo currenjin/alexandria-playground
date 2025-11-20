dc0f4dbed849723c0fa984434e46d1b80687e9bd
currenjin <hyun0524e@naver.com>
2025-11-20 23:45:15 +0900

    improve: add user-service.url for UserClient

--- book-building-microservices/examples/phase2-migration/monolith/src/main/java/com/currenjin/music/client/user/impl/UserClientImpl.java
+++ book-building-microservices/examples/phase2-migration/monolith/src/main/java/com/currenjin/music/client/user/impl/UserClientImpl.java
@@ -2,6 +2,7 @@ package com.currenjin.music.client.user.impl;
 
 import com.currenjin.music.client.user.UserClient;
 import com.currenjin.music.client.user.dto.UserDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
@@ -11,11 +12,14 @@ import org.springframework.web.client.RestTemplate;
 public class UserClientImpl implements UserClient {
     private RestTemplate restTemplate;
 
+    @Value("${target.user-service.url}")
+    private String baseUrl;
+
     @Override
     public Boolean userExists(Long userId) {
         try {
             ResponseEntity<UserDto> response = restTemplate.getForEntity(
-                    "http://localhost:8080/users/" + userId,
+                    baseUrl + "/api/users/" + userId,
                     UserDto.class);
 
             return response.getStatusCode().is2xxSuccessful();
