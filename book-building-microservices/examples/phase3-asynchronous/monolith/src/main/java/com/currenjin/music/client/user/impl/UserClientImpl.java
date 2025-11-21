1ef3faf67e41a04e9d0148dc7aea65d569e8cea6
currenjin <hyun0524e@naver.com>
2025-11-21 22:04:01 +0900

    fix: RestTemplateConfig

--- book-building-microservices/examples/phase3-asynchronous/monolith/src/main/java/com/currenjin/music/client/user/impl/UserClientImpl.java
+++ book-building-microservices/examples/phase3-asynchronous/monolith/src/main/java/com/currenjin/music/client/user/impl/UserClientImpl.java
@@ -1,21 +1,26 @@
 package com.currenjin.music.client.user.impl;
 
-import com.currenjin.music.client.user.UserClient;
-import com.currenjin.music.client.user.dto.UserDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import com.currenjin.music.client.user.UserClient;
+import com.currenjin.music.client.user.dto.UserDto;
+
 @Component
 public class UserClientImpl implements UserClient {
     private RestTemplate restTemplate;
 
+	public UserClientImpl(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
     @Value("${target.user-service.url}")
     private String baseUrl;
 
-    @Override
+	@Override
     public Boolean userExists(Long userId) {
         try {
             ResponseEntity<UserDto> response = restTemplate.getForEntity(
