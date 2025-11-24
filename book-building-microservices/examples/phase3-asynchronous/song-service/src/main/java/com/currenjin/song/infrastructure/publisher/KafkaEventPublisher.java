9c4b5f67d373df1e9ab4232224c46683b6855a24
currenjin <hyun0524e@naver.com>
2025-11-24 22:15:38 +0900

    feat: KafkaEventPublisher

---
+++ book-building-microservices/examples/phase3-asynchronous/song-service/src/main/java/com/currenjin/song/infrastructure/publisher/KafkaEventPublisher.java
@@ -0,0 +1,21 @@
+package com.currenjin.song.infrastructure.publisher;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import com.currenjin.song.domain.event.DomainEvent;
+import com.currenjin.song.service.publisher.EventPublisher;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaEventPublisher implements EventPublisher {
+
+	private final KafkaTemplate<String, Object> kafkaTemplate;
+
+	@Override
+	public void publish(DomainEvent event) {
+		kafkaTemplate.send(event.getTopic(), event);
+	}
+}
