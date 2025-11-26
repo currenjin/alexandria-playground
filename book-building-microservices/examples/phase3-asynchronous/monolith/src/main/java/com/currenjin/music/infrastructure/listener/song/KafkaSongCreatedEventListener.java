6d2394108e343cdafb5a7712566b23c0ce22ce58
currenjin <hyun0524e@naver.com>
2025-11-26 12:46:25 +0900

    feat: implement onEvent

--- book-building-microservices/examples/phase3-asynchronous/monolith/src/main/java/com/currenjin/music/infrastructure/listener/song/KafkaSongCreatedEventListener.java
+++ book-building-microservices/examples/phase3-asynchronous/monolith/src/main/java/com/currenjin/music/infrastructure/listener/song/KafkaSongCreatedEventListener.java
@@ -2,6 +2,9 @@ package com.currenjin.music.infrastructure.listener.song;
 
 import com.currenjin.music.infrastructure.listener.EventListener;
 import com.currenjin.music.share.DomainEvent;
+import com.currenjin.music.share.song.SongCreatedEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,11 +13,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaSongCreatedEventListener implements EventListener {
     private final Logger log = LoggerFactory.getLogger(getClass());
+    private final ObjectMapper objectMapper;
+
+    public KafkaSongCreatedEventListener(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
 
     // FIXME: groupId, topics, handle messages
     @Override
     @KafkaListener(topics = "SongCreatedEvent", groupId = "song-service-group")
-    public void onEvent(DomainEvent event) {
-        log.info("onEvent: {}, occurredAt: {}", event.getType(), event.getOccurredAt());
+    public void onEvent(String event) {
+        try {
+            SongCreatedEvent createdEvent = objectMapper.readValue(event, SongCreatedEvent.class);
+            log.info("onEvent: {}, occurredAt: {}", createdEvent.getType(), createdEvent.getOccurredAt());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to deserialize DomainEvent", e);
+        }
     }
 }
