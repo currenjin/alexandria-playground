40089c4090c1ab2a01c0a878396cb2fa7b7ad050
currenjin <hyun0524e@naver.com>
2025-12-01 09:51:07 +0900

    init: mafia server

---
+++ playground-mafia-server/src/main/kotlin/com/currenjin/mafia/KotlinPlaygroundApplication.kt
@@ -0,0 +1,7 @@
+package com.currenjin.mafia
+
+class KotlinPlaygroundApplication
+
+fun main(args: Array<String>) {
+    println("Kotlin Application Started")
+}
---
+++ playground-mafia-server/src/test/kotlin/com/currenjin/mafia/KotlinPlaygroundApplicationTests.kt
@@ -0,0 +1,9 @@
+package com.currenjin.mafia
+
+import org.junit.jupiter.api.Test
+
+class KotlinPlaygroundApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
