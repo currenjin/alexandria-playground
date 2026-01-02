eabe9cde3cba6f2c13944d21135f5d0371d3fe96
currenjin <hyun0524e@naver.com>
2026-01-02 22:10:01 +0900

    challenge-260102: 3 returns 2

--- poc-challenges/260102/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
+++ poc-challenges/260102/src/main/kotlin/com/currenjin/tdd/Fibonacci.kt
@@ -3,7 +3,9 @@ package com.currenjin.tdd
 class Fibonacci {
     companion object {
         fun fib(i: Int): Int {
-            return 1
+            if (i <= 2) return 1
+
+            return 2
         }
     }
 }
