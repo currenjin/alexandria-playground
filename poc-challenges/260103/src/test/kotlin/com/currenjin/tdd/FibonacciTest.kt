403c084b6ffdfe54709adb9b9e597d3df2647982
currenjin <hyun0524e@naver.com>
2026-01-03 22:15:54 +0900

    challenge-260103: 4 returns 3

--- poc-challenges/260103/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
+++ poc-challenges/260103/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
@@ -6,7 +6,7 @@ import kotlin.test.assertEquals
 
 class FibonacciTest {
     @ParameterizedTest
-    @CsvSource(value = ["1,1", "2,1", "3,2"])
+    @CsvSource(value = ["1,1", "2,1", "3,2", "4,3"])
     fun fibonacci_test(actual: Int, expected: Int) {
         assertEquals(expected, Fibonacci.fib(actual))
     }
