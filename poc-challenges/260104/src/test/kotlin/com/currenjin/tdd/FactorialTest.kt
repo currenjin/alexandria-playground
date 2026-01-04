0221694202ddd3c9a5964d289f929fc258b159d1
currenjin <hyun0524e@naver.com>
2026-01-04 23:19:07 +0900

    challenge-260104: 1 returns 1

--- poc-challenges/260104/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
+++ poc-challenges/260104/src/test/kotlin/com/currenjin/tdd/FactorialTest.kt
@@ -1,4 +1,11 @@
 package com.currenjin.tdd
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class FactorialTest {
+    @Test
+    fun factorial_test() {
+        assertEquals(1, Factorial.fac(1))
+    }
 }
