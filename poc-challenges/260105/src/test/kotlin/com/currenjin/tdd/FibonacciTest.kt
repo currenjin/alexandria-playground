9e1224a73a4062f46bfa76984ddca7aeeec791a5
currenjin <hyun0524e@naver.com>
2026-01-05 21:17:49 +0900

    challenge-260105: 1 returns 1

--- poc-challenges/260105/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
+++ poc-challenges/260105/src/test/kotlin/com/currenjin/tdd/FibonacciTest.kt
@@ -1,4 +1,11 @@
 package com.currenjin.tdd
 
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
 class FibonacciTest {
+    @Test
+    fun fibonacci() {
+        assertEquals(1, Fibonacci.fib(1))
+    }
 }
