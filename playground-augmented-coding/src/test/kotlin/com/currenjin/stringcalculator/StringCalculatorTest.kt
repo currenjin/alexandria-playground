7895ec5ae88091ca5e76f2fe681169aa7ced28b1
currenjin <hyun0524e@naver.com>
2026-01-12 17:16:10 +0900

    Augmented Coding: 빈 문자열  → 0 반환

--- playground-augmented-coding/src/test/kotlin/com/currenjin/stringcalculator/StringCalculatorTest.kt
+++ playground-augmented-coding/src/test/kotlin/com/currenjin/stringcalculator/StringCalculatorTest.kt
@@ -1,10 +1,12 @@
 package com.currenjin.stringcalculator
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class StringCalculatorTest {
- @Test
- fun name() {
-  TODO("Not yet implemented")
- }
+    @Test
+    fun `빈 문자열은 0을 반환한다`() {
+        val calculator = StringCalculator()
+        assertEquals(0, calculator.add(""))
+    }
 }
