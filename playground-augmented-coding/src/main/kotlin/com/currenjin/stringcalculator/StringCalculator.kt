c6c8baeb9cab090f19c1cb65cee3a2c7d3a7eb99
currenjin <hyun0524e@naver.com>
2026-01-12 17:20:49 +0900

    Augmented Coding: 쉼표 외에 줄바꿈(\n)도 구분자로 허용

--- playground-augmented-coding/src/main/kotlin/com/currenjin/stringcalculator/StringCalculator.kt
+++ playground-augmented-coding/src/main/kotlin/com/currenjin/stringcalculator/StringCalculator.kt
@@ -3,6 +3,6 @@ package com.currenjin.stringcalculator
 class StringCalculator {
     fun add(numbers: String): Int {
         if (numbers.isEmpty()) return 0
-        return numbers.split(",").sumOf { it.toInt() }
+        return numbers.split(",", "\n").sumOf { it.toInt() }
     }
 }
