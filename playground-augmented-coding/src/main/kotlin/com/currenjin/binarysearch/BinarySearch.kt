0568561b1a4be86032f0da73313fe6a5f4cd0623
currenjin <hyun0524e@naver.com>
2026-01-13 17:55:23 +0900

    Augmented Coding: 단일 요소 배열에서 해당 값 검색 -> Found(0) 반환

--- playground-augmented-coding/src/main/kotlin/com/currenjin/binarysearch/BinarySearch.kt
+++ playground-augmented-coding/src/main/kotlin/com/currenjin/binarysearch/BinarySearch.kt
@@ -2,6 +2,9 @@ package com.currenjin.binarysearch
 
 object BinarySearch {
     fun <T : Comparable<T>> search(list: List<T>, target: T): BinarySearchResult<T> {
+        if (list.isNotEmpty() && list[0] == target) {
+            return BinarySearchResult.Found(0)
+        }
         return BinarySearchResult.NotFound(0)
     }
 }
