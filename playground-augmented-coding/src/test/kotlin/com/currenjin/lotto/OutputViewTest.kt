86159d0dee7419691d75d6e0dc1aa90f56e833f9
currenjin <hyun0524e@naver.com>
2026-01-24 13:08:41 +0900

    Augmented Coding: 당첨 통계 출력

--- playground-augmented-coding/src/test/kotlin/com/currenjin/lotto/OutputViewTest.kt
+++ playground-augmented-coding/src/test/kotlin/com/currenjin/lotto/OutputViewTest.kt
@@ -24,4 +24,28 @@ class OutputViewTest {
         assertThat(result).contains("[1, 2, 3, 4, 5, 6]")
         assertThat(result).contains("[7, 8, 9, 10, 11, 12]")
     }
+
+    @Test
+    fun `should print winning statistics`() {
+        val output = ByteArrayOutputStream()
+        System.setOut(PrintStream(output))
+
+        val lottos = listOf(
+            LottoGenerator.create(listOf(1, 2, 3, 43, 44, 45)) // FIFTH
+        )
+        val winningLotto = WinningLotto(
+            LottoGenerator.create(listOf(1, 2, 3, 4, 5, 6)),
+            LottoNumber(7)
+        )
+        val lottoResult = LottoResult(lottos, winningLotto)
+
+        OutputView.printStatistics(lottoResult)
+
+        val result = output.toString()
+        assertThat(result).contains("3개 일치 (5000원)- 1개")
+        assertThat(result).contains("4개 일치 (50000원)- 0개")
+        assertThat(result).contains("5개 일치 (1500000원)- 0개")
+        assertThat(result).contains("5개 일치, 보너스 볼 일치(30000000원) - 0개")
+        assertThat(result).contains("6개 일치 (2000000000원)- 0개")
+    }
 }
