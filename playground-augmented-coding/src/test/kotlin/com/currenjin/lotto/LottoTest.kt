7576a749c303976e779334be70b7381d0377f03c
currenjin <hyun0524e@naver.com>
2026-01-24 12:31:07 +0900

    Augmented Coding: 6개 초과의 숫자로 생성 시 예외 발생

--- playground-augmented-coding/src/test/kotlin/com/currenjin/lotto/LottoTest.kt
+++ playground-augmented-coding/src/test/kotlin/com/currenjin/lotto/LottoTest.kt
@@ -36,4 +36,21 @@ class LottoTest {
             )
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
+
+    @Test
+    fun `should throw exception when more than 6 numbers`() {
+        assertThatThrownBy {
+            Lotto(
+                listOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(6),
+                    LottoNumber(7)
+                )
+            )
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
