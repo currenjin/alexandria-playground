b1c31ec10dbaa05eae2cc7d3e872a20136cf482b
currenjin <hyun0524e@naver.com>
2026-01-24 12:54:04 +0900

    Augmented Coding: 1,000원 미만 구매 시 예외 발생

--- playground-augmented-coding/src/main/kotlin/com/currenjin/lotto/LottoStore.kt
+++ playground-augmented-coding/src/main/kotlin/com/currenjin/lotto/LottoStore.kt
@@ -2,6 +2,11 @@ package com.currenjin.lotto
 
 class LottoStore {
     fun purchase(amount: Int): List<Lotto> {
+        require(amount >= LOTTO_PRICE)
         return listOf(LottoGenerator.generate())
+    }
+
+    companion object {
+        private const val LOTTO_PRICE = 1000
     }
 }
