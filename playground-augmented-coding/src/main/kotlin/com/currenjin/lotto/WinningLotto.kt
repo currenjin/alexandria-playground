cd18ece71c607ba8b8c971218de9b467bae8af8f
currenjin <hyun0524e@naver.com>
2026-01-24 12:42:32 +0900

    Augmented Coding: Lotto와 비교하여 일치 개수 반환

--- playground-augmented-coding/src/main/kotlin/com/currenjin/lotto/WinningLotto.kt
+++ playground-augmented-coding/src/main/kotlin/com/currenjin/lotto/WinningLotto.kt
@@ -4,4 +4,6 @@ class WinningLotto(val lotto: Lotto, val bonus: LottoNumber) {
     init {
         require(!lotto.contains(bonus))
     }
+
+    fun matchCount(userLotto: Lotto): Int = lotto.matchCount(userLotto)
 }
