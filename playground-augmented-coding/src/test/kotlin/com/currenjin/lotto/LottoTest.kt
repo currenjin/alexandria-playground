8bca58d310f355b3a5cff2eb86a2a1550ed5f884
currenjin <hyun0524e@naver.com>
2026-01-24 12:32:00 +0900

    Augmented Coding: 중복된 숫자가 있으면 예외 발생

--- playground-augmented-coding/src/test/kotlin/com/currenjin/lotto/LottoTest.kt
+++ playground-augmented-coding/src/test/kotlin/com/currenjin/lotto/LottoTest.kt
@@ -53,4 +53,20 @@ class LottoTest {
             )
         }.isInstanceOf(IllegalArgumentException::class.java)
     }
+
+    @Test
+    fun `should throw exception when numbers contain duplicates`() {
+        assertThatThrownBy {
+            Lotto(
+                listOf(
+                    LottoNumber(1),
+                    LottoNumber(2),
+                    LottoNumber(3),
+                    LottoNumber(4),
+                    LottoNumber(5),
+                    LottoNumber(5)
+                )
+            )
+        }.isInstanceOf(IllegalArgumentException::class.java)
+    }
 }
