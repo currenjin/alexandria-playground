3acfd46645c6b64c1a807db3056b3b2da1f1cc78
currenjin <hyun0524e@naver.com>
2026-01-24 12:36:22 +0900

    Augmented Coding: 1~45 중 랜덤한 6개 숫자로 Lotto 생성

---
+++ playground-augmented-coding/src/test/kotlin/com/currenjin/lotto/LottoGeneratorTest.kt
@@ -0,0 +1,15 @@
+package com.currenjin.lotto
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LottoGeneratorTest {
+
+    @Test
+    fun `should generate lotto with 6 random numbers between 1 and 45`() {
+        val lotto = LottoGenerator.generate()
+
+        assertThat(lotto.numbers).hasSize(6)
+        assertThat(lotto.numbers.map { it.value }).allMatch { it in 1..45 }
+    }
+}
