c33470136a486019f439608a8a3e4ea3d3609253
currenjin <hyun0524e@naver.com>
2026-01-24 13:08:41 +0900

    Augmented Coding: 당첨 통계 출력

--- playground-augmented-coding/src/main/kotlin/com/currenjin/lotto/OutputView.kt
+++ playground-augmented-coding/src/main/kotlin/com/currenjin/lotto/OutputView.kt
@@ -7,4 +7,14 @@ object OutputView {
             println(lotto.numbers.map { it.value }.sorted())
         }
     }
+
+    fun printStatistics(result: LottoResult) {
+        println("당첨 통계")
+        println("---------")
+        println("3개 일치 (5000원)- ${result.countByRank(Rank.FIFTH)}개")
+        println("4개 일치 (50000원)- ${result.countByRank(Rank.FOURTH)}개")
+        println("5개 일치 (1500000원)- ${result.countByRank(Rank.THIRD)}개")
+        println("5개 일치, 보너스 볼 일치(30000000원) - ${result.countByRank(Rank.SECOND)}개")
+        println("6개 일치 (2000000000원)- ${result.countByRank(Rank.FIRST)}개")
+    }
 }
